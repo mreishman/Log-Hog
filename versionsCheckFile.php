d7fcafa3878b296a52746cd40020f38fc04bee07
mreishman <matt.reishman@me.com>
2017-03-07 19:24:55 -0600

    temp test

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -2,5 +2,12 @@
 
 $versionCheckArray = array(
 	'version'		=> '2.0'
+	'versionList'		=> array(
+		'2.0.1'	        => '2Point0point1'
+	),
+	'2Point0point1'			=> array(
+		'branchName'	=> 'testUpdate',
+		'releaseNotes'	=> '<ul><li>Test Update</li><ul>'
+	)
 );
 ?>
