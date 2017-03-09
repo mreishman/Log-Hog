c87deb103198d03385dca04fe4af47547bbd5cec
Matt Reishman <mreishman@users.noreply.github.com>
2017-03-09 13:03:07 -0600

    Update updateScript.php

--- updateScript.php
+++ updateScript.php
@@ -2,7 +2,10 @@
   
   //update script do things
   
-  
+ //for testing this will just update status
+
+updateProgressFile("Finished Running Update Script", "../core/php/", "updateProgressFileNext.php", "");
+updateProgressFile("Finished Running Update Script", "../core/php/", "updateProgressFile.php", "");
   
   
 ?>
