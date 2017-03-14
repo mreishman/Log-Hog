e92e10df84899d58105c91618dd8a2f36f2888c3
Matt Reishman <matt.reishman@Me.com>
2017-03-14 08:43:25 -0500

    2.0.1 Update to configStatic
    2.0.1 Update to config static

--- core/php/configStatic.php
+++ core/php/configStatic.php
@@ -2,9 +2,9 @@
 <?php
 
 $configStatic = array(
-  'version'   => '2.0',
-  'lastCheck'   => '03-10-2017',
-  'newestVersion' => '2.0',
+  'version'   => '2.0.1',
+  'lastCheck'   => '03-14-2017',
+  'newestVersion' => '2.0.1',
   'versionList' => array(
   '2.0.1' => array('branchName' => 'testUpdate','releaseNotes' => '<ul><li>Test Update</li><ul>')
   )
