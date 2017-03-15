3b196a661503b2c23b98b04360df65467573143a
Matt Reishman <mreishman@users.noreply.github.com>
2017-03-15 09:01:57 -0500

    Update updateScript.php

--- updateScript.php
+++ updateScript.php
@@ -1,11 +1,51 @@
 <?php
   
-  //update script do things
-  
- //for testing this will just update status
+$arrayOfFiles = $array ("core_js_main.js", "core_php_settingsCheckForUpdate.php","settings_main.php","settings_update.php");
 
-updateProgressFile("Finished Running Update Script", "", "updateProgressFileNext.php", "");
-updateProgressFile("Finished Running Update Script", "", "updateProgressFile.php", "");
+require_once(innerUpgradeStatus.php);
+
+if($innerUpdateProgress['currentFile'] < sizeOf($arrayOfFiles))
+{
+ 
+$currentFile = $arrayOfFiles[$innerUpdateProgress['currentFile']]; 
   
+//update innerUpgradeStatus file
+$newCount = $innerUpdateProgress['currentFile'] + 1;
+
+$currentFileArray = explode("_", $currentFile );  
+$sizeOfCurrentFileArray = sizeOf($currentFileArray);
+
+$nameOfFile = $currentFileArray[$sizeOfCurrentFileArray - 1];
+
+$directoryPath = "../../";
+  
+for($i = 0; $i < $sizeOfCurrentFileArray - 1; $i++)
+{
+  $directoryPath .= $currentFileArray[$i]."/"; 
+}
+
+$newFile = $directoryPath.$nameOfFile;
+
+file_put_contents($newFile, file_get_contents("../../update/downloads/updateFiles/extracted/".$currentFile));  
+  
+$string = "Updating file ".$newCount." of ".$sizeOf($arrayOfFiles). " "; 
+  
+//update message for update  
+  
+updateProgressFile($string, "", "updateProgressFileNext.php", "");
+updateProgressFile($string, "", "updateProgressFile.php", "");   
+  
+  
+}
+else
+{
+  
+updateProgressFile("Finished Running Update Script", "", "updateProgressFileNext.php", "");
+updateProgressFile("Finished Running Update Script", "", "updateProgressFile.php", "");  
+  
+}
+
+
+
   
 ?>
