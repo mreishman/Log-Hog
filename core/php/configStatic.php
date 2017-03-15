4d18a5c21a767f2574785816548a556256b41b31
Matt Reishman <matt.reishman@Me.com>
2017-03-15 13:59:42 -0500

    2.0.1 Updated

--- core/php/configStatic.php
+++ core/php/configStatic.php
@@ -3,9 +3,9 @@
 
 $configStatic = array(
   'version'   => '2.0.1',
-  'lastCheck'   => '03-14-2017',
+  'lastCheck'   => '03-15-2017',
   'newestVersion' => '2.0.1',
   'versionList' => array(
-  '2.0.1' => array('branchName' => 'testUpdate','releaseNotes' => '<ul><li>Test Update</li><ul>')
+  '2.0.1' => array('branchName' => '2.0.1Update','releaseNotes' => '<ul><li>Bug Fixes<ul><li>Adds check for header redirect</li><li>Fixed Unpause - on focus (if default paused) Bug</li><li>Adds remove directory / watch folder button<li></ul></li><ul>')
   )
 );
