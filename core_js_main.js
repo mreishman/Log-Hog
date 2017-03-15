db14b57c08e45a26e773443ffa3632ba55253afe
Matt Reishman <mreishman@users.noreply.github.com>
2017-03-15 08:36:13 -0500

    Added upgraded files

---
+++ core_js_main.js
@@ -0,0 +1,303 @@
+var title = $('title').text();
+var currentPage;
+var logs = {};
+var titles = {};
+var lastLogs = {};
+var fresh = true;
+var flasher;
+var updating = false;
+var startedPauseOnNonFocus = false;
+
+function poll() {
+
+	if(!startedPauseOnNonFocus)
+	{
+		startPauseOnNotFocus();
+	}
+
+	if (autoCheckUpdate == true && !updating)
+	{
+		var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth()+1; //January is 0!
+		var yyyy = today.getFullYear();
+
+		if(dd<10) {
+		    dd='0'+dd
+		} 
+
+		if(mm<10) {
+		    mm='0'+mm
+		} 
+
+		today = mm+'-'+dd+'-'+yyyy;
+		if(today != dateOfLastUpdate)
+		{
+			updating = true;
+			window.location.href = "core/php/settingsCheckForUpdate.php";
+		}
+		else
+		{
+			pollTwo();
+		}
+
+	}
+	else
+	{
+		pollTwo();
+	}
+
+	
+}
+
+function pollTwo()
+{
+	if(!pausePoll)
+	{
+		if(refreshing)
+		{
+			document.title = "Log Hog | Refreshing";
+		}
+		else
+		{
+			document.title = "Log Hog | Index";
+		}
+	$.getJSON('core/php/poll.php', {}, function(data) {
+		update(data);
+		fresh = false;
+	});
+	}
+}
+
+function pausePollAction()
+{
+	if(pausePoll)
+	{
+		userPaused = false;
+		pausePoll = false;
+		document.getElementById('pauseImage').src="core/img/Pause.png";
+	}
+	else
+	{
+		userPaused = true;
+		pausePollFunction();
+	}
+}
+
+function refreshAction()
+{
+	clearTimeout(refreshActionVar);
+	document.getElementById('refreshImage').src="core/img/refresh-animated.gif";
+	refreshing = true;
+	if(pausePoll)
+	{
+		clearTimeout(refreshPauseActionVar);
+		pausePoll = false;
+		poll();
+		refreshPauseActionVar = setTimeout(function(){pausePoll = true;}, 1000);
+	}
+	else
+	{
+		poll();
+	}
+	refreshActionVar = setTimeout(function(){endRefreshAction()}, 1500);
+}
+
+function endRefreshAction()
+{
+	document.getElementById('refreshImage').src="core/img/Refresh.png"; 
+	refreshing = false;
+	if(pausePoll)
+	{
+		document.title = "Log Hog | Paused";
+	}
+	else
+	{
+		document.title = "Log Hog | Index";
+	}
+}
+
+function update(data) {
+	var menu = $('#menu');
+	var blank = $('#storage .menuItem').html();
+	var i, id, name, shortName, item;
+	var files = Object.keys(data);
+	var stop = files.length;
+	var updated = false;
+	var initialized = $('#menu a').length != 0;
+	
+	for(i = 0; i != stop; ++i) {
+		name = files[i];
+		id = name.replace(/[^a-z0-9]/g, '');
+		logs[id] = data[name];
+		
+		if($('#menu .' + id + 'Button').length == 0) {
+			titles[id] = name;
+			shortName = files[i].replace(/.*\//g, '');
+			item = blank;
+			item = item.replace(/{{title}}/g, shortName);
+			item = item.replace(/{{id}}/g, id);
+			menu.append(item);
+		}
+		
+		if(logs[id] != lastLogs[id]) {
+			updated = true;
+			if(id == currentPage)
+				$('#log').html(makePretty(logs[id]));
+			else if(!fresh && !$('#menu a.' + id + 'Button').hasClass('updated'))
+				$('#menu a.' + id + 'Button').addClass('updated');
+		}
+		
+		if(initialized && updated && $(window).filter(':focus').length == 0) {
+			flashTitle();
+		}
+	}
+	resize();
+	
+	if($('#menu .active').length == 0)
+		$('#menu a:eq(0)').click();
+	
+	if(logs[currentPage] != lastLogs[currentPage]) {
+		lastLogs[currentPage] = logs[currentPage];
+		document.getElementById('main').scrollTop = $('#log').outerHeight();
+	}
+	
+	var ids = Object.keys(logs);
+	for(i = 0; i != stop; ++i) {
+		id = ids[i];
+		lastLogs[id] = logs[id];
+	}
+}
+
+function show(e, id) {
+	$(e).siblings().removeClass('active');
+	$(e).addClass('active').removeClass('updated');
+	$('#log').html(makePretty(logs[id]));
+	currentPage = id;
+	$('#title').html(titles[id]);
+	document.getElementById('main').scrollTop = $('#log').outerHeight();
+}
+
+function makePretty(text) {
+	text = text.split("\n");
+	text = text.join('</div><div>');
+	
+	return '<div>' + text + '</div>';
+}
+
+function resize() {
+	var targetHeight = window.innerHeight - $('#menu').outerHeight() - $('#title').outerHeight();
+	if($('#main').outerHeight() != targetHeight)
+		$('#main').outerHeight(targetHeight);
+	if($('#main').css('bottom') != $('#title').outerHeight() + 'px')
+		$('#main').css('bottom', $('#title').outerHeight() + 'px');
+}
+
+function flashTitle() {
+	stopFlashTitle();
+	$('title').text('');
+	flasher = setInterval(function() {
+		$('title').text($('title').text() == '' ? title : '');
+	}, 1000);
+}
+
+function stopFlashTitle() {
+	clearInterval(flasher);
+	$('title').text(title);
+}
+
+function focus() {
+	stopFlashTitle();
+}
+
+
+
+poll();
+if (autoCheckUpdate == true)
+{
+	var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth()+1; //January is 0!
+		var yyyy = today.getFullYear();
+
+		if(dd<10) {
+		    dd='0'+dd
+		} 
+
+		if(mm<10) {
+		    mm='0'+mm
+		} 
+
+		today = mm+'-'+dd+'-'+yyyy;
+		if(today != dateOfLastUpdate && !updating)
+		{
+			updating = true;
+			window.location.href = "core/php/settingsCheckForUpdate.php";
+		}
+		else
+		{
+			setInterval(poll, pollingRate);
+		}
+}
+else
+{
+	setInterval(poll, pollingRate);
+}
+setInterval(poll, pollingRate);
+resize();
+
+window.onresize = resize;
+window.onfocus = focus;
+
+if(pausePollFromFile)
+{
+	pausePoll = true;
+	document.getElementById('pauseImage').src="core/img/Play.png";
+}
+
+if(pausePollOnNotFocus && !pausePollFromFile)
+{
+	startPauseOnNotFocus();
+}
+
+function startPauseOnNotFocus()
+{
+	startedPauseOnNonFocus = true;
+	Visibility.every(250, 1000, function () { checkIfPageHidden(); });
+}
+
+function checkIfPageHidden()
+{
+	if(isPageHidden())
+	{
+		//hidden
+		if(!pausePoll)
+		{
+			pausePollFunction();
+		}
+	}
+	else
+	{
+		//not hidden
+		if(!userPaused && pausePoll)
+		{
+			pausePoll = false;
+			document.getElementById('pauseImage').src="core/img/Pause.png";
+		}
+		if(userPaused)
+		{
+			document.title = "Log Hog | Paused";
+		}
+	}
+}
+
+function pausePollFunction()
+{
+	pausePoll = true;
+	document.getElementById('pauseImage').src="core/img/Play.png";
+	document.title = "Log Hog | Paused";
+}
+
+function isPageHidden(){
+     return document.hidden || document.msHidden || document.webkitHidden || document.mozHidden;
+ }
---
+++ settings_main.php
@@ -0,0 +1,240 @@
+<?php
+$baseUrl = "../core/";
+if(file_exists('../local/layout.php'))
+{
+	$baseUrl = "../local/";
+	//there is custom information, use this
+	require_once('../local/layout.php');
+	$baseUrl .= $currentSelectedTheme."/";
+}
+require_once($baseUrl.'conf/config.php'); 
+require_once('../core/conf/config.php');
+require_once('../core/php/configStatic.php');
+
+$version = explode('.', $configStatic['version']);
+$newestVersion = explode('.', $configStatic['newestVersion']);
+
+$levelOfUpdate = 0; // 0 is no updated, 1 is minor update and 2 is major update
+
+$newestVersionCount = count($newestVersion);
+$versionCount = count($version);
+
+for($i = 0; $i < $newestVersionCount; $i++)
+{
+	if($i < $versionCount)
+	{
+		if($i == 0)
+		{
+			if($newestVersion[$i] > $version[$i])
+			{
+				$levelOfUpdate = 3;
+				break;
+			}
+			elseif($newestVersion[$i] < $version[$i])
+			{
+				break;
+			}
+		}
+		elseif($i == 1)
+		{
+			if($newestVersion[$i] > $version[$i])
+			{
+				$levelOfUpdate = 2;
+				break;
+			}
+			elseif($newestVersion[$i] < $version[$i])
+			{
+				break;
+			}
+		}
+		else
+		{
+			if($newestVersion[$i] > $version[$i])
+			{
+				$levelOfUpdate = 1;
+				break;
+			}
+			elseif($newestVersion[$i] < $version[$i])
+			{
+				break;
+			}
+		}
+	}
+	else
+	{
+		$levelOfUpdate = 1;
+		break;
+	}
+}
+
+if(array_key_exists('developmentTabEnabled', $config))
+{
+	$developmentTabEnabled = $config['developmentTabEnabled'];
+}
+else
+{
+	$developmentTabEnabled = $defaultConfig['developmentTabEnabled'];
+} 
+
+?>
+<!doctype html>
+<head>
+	<title>Log Hog | Settings</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl ?>template/theme.css">
+	<link rel="icon" type="image/png" href="../core/img/favicon.png" />
+	<script src="../core/js/jquery.js"></script>
+</head>
+<body>
+	<div id="menu">
+		<div onclick="window.location.href = '../index.php'" style="display: inline-block; cursor: pointer; height: 30px; width: 30px; ">
+			<img id="pauseImage" class="menuImage" src="../core/img/backArrow.png" height="30px">
+		</div>
+		<a class="active" >Main</a>
+		<a onclick="window.location.href = 'about.php';">About</a>
+		<a onclick="window.location.href = 'update.php';"><?php  if($levelOfUpdate == 1){echo '<img src="../core/img/yellowWarning.png" height="10px">';} ?> <?php if($levelOfUpdate == 2){echo '<img src="../core/img/redWarning.png" height="10px">';} ?>Update</a>
+		<a onclick="window.location.href = 'advanced.php';">Advanced</a>
+		<?php
+		if($developmentTabEnabled == 'true'):?>
+			<a onclick="window.location.href = 'devTools.php';"> Dev Tools </a>
+		<?php endif; ?>
+	</div>
+	
+<?php
+if(array_key_exists('sliceSize', $config))
+{
+	$sliceSize = $config['sliceSize'];
+}
+else
+{
+	$sliceSize = $defaultConfig['sliceSize'];
+} 
+if(array_key_exists('pollingRate', $config))
+{
+	$pollingRate = $config['pollingRate'];
+}
+else
+{
+	$pollingRate = $defaultConfig['pollingRate'];
+} 
+if(array_key_exists('pausePoll', $config))
+{
+	$pausePoll = $config['pausePoll'];
+}
+else
+{
+	$pausePoll = $defaultConfig['pausePoll'];
+}
+if(array_key_exists('pauseOnNotFocus', $config))
+{
+	$pauseOnNotFocus = $config['pauseOnNotFocus'];
+}
+else
+{
+	$pauseOnNotFocus = $defaultConfig['pauseOnNotFocus'];
+}
+if(array_key_exists('autoCheckUpdate', $config))
+{
+	$autoCheckUpdate = $config['autoCheckUpdate'];
+}
+else
+{
+	$autoCheckUpdate = $defaultConfig['autoCheckUpdate'];
+}
+?>
+	
+	
+
+	<div id="main">
+		<form id="settingsMainVars" action="../core/php/settingsMainUpdateVars.php" method="post">
+		<div class="settingsHeader">
+		Main Settings <button>Save Changes</button>
+		</div>
+		<div class="settingsDiv" >
+		<ul id="settingsUl">
+			<li>
+				sliceSize:  <input type="text" name="sliceSize" value="<?php echo $sliceSize;?>" >
+			</li>
+			<li>
+				pollingRate: <input type="text" name="pollingRate" value="<?php echo $pollingRate;?>" >
+			</li>
+			<li>
+				pausePoll: 
+					<select name="pausePoll">
+  						<option <?php if($pausePoll == 'true'){echo "selected";} ?> value="true">True</option>
+  						<option <?php if($pausePoll == 'false'){echo "selected";} ?> value="false">False</option>
+					</select>
+			</li>
+			<li>
+				pauseOnNotFocus:
+					<select name="pauseOnNotFocus">
+  						<option <?php if($pauseOnNotFocus == 'true'){echo "selected";} ?> value="true">True</option>
+  						<option <?php if($pauseOnNotFocus == 'false'){echo "selected";} ?> value="false">False</option>
+					</select>
+			</li>
+			<li>
+				autoCheckUpdate:
+					<select name="autoCheckUpdate">
+  						<option <?php if($autoCheckUpdate == 'true'){echo "selected";} ?> value="true">True</option>
+  						<option <?php if($autoCheckUpdate == 'false'){echo "selected";} ?> value="false">False</option>
+					</select>
+			</li>
+		</ul>
+		</div>
+		</form>
+		<form id="settingsMainWatch" action="../core/php/settingsMainUpdateWatchList.php" method="post">
+		<div class="settingsHeader">
+			WatchList <button>Save Changes</button>
+		</div>
+		<div class="settingsDiv" >	
+		<ul id="settingsUl">
+			<?php 
+				$i = 0;
+				foreach($config['watchList'] as $key => $item): $i++; ?>
+			<li id="rowNumber<?php echo $i; ?>" >
+				File #<?php if($i < 10){echo "0";} ?><?php echo $i; ?>: 
+ 				<input style='width: 500px;' type='text' name='watchListKey<?php echo $i; ?>' value='<?php echo $key; ?>'>
+ 				<input type='text' name='watchListItem<?php echo $i; ?>' value='<?php echo $item; ?>'>
+ 				<a style="cursor: pointer;" onclick="deleteRowFunction(<?php echo $i; ?>)">Remove File / Folder</a>
+			</li>
+
+		<?php endforeach; ?>
+		<div id="newRowLocationForWatchList">
+		</div>
+		</ul>
+		<ul id="settingsUl">
+			<li>
+				<a style="cursor: pointer;" onclick="addRowFunction()">Add New File / Folder</a>
+			</li>
+		</ul>
+		</div>
+		<div id="hidden" style="display: none">
+			<input id="numberOfRows" type="text" name="numberOfRows" value="<?php echo $i;?>">
+		</div>	
+		</form>
+	</div>	
+</body>
+<script src="../core/js/settings.js"></script>
+<script type="text/javascript"> 
+var countOfWatchList = <?php echo $i; ?>
+
+function addRowFunction()
+{
+	countOfWatchList++;
+	if(countOfWatchList < 10)
+	{
+		document.getElementById('newRowLocationForWatchList').innerHTML += "<li>File #0" + countOfWatchList+ ": <input type='text' style='width: 500px;' name='watchListKey" + countOfWatchList + "' > <input type='text' name='watchListItem" + countOfWatchList + "' ></li>";
+	}
+	else
+	{
+		document.getElementById('newRowLocationForWatchList').innerHTML += "<li>File #" + countOfWatchList+ ": <input type='text' style='width: 500px;' name='watchListKey" + countOfWatchList + "' > <input type='text' name='watchListItem" + countOfWatchList + "' ></li>";
+	}
+	document.getElementById('numberOfRows').value = countOfWatchList;
+}
+
+function deleteRowFunction(currentRow)
+{
+	var elementToFind = "rowNumber" + currentRow;
+	document.getElementById(elementToFind).outerHTML = "";
+}	
+
+</script>
