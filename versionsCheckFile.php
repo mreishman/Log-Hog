27b8ad63ff0c56c9f63b23adb1036d77524c8b15
Matt Reishman <mreishman@users.noreply.github.com>
2017-03-15 13:53:09 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -1,11 +1,11 @@
 <?php
 
 $versionCheckArray = array(
-	'version'		=> '2.0',
+	'version'		=> '2.0.1',
 	'versionList'		=> array(
 		'2.0.1'	        => array(
-			'branchName'	=> 'testUpdate',
-			'releaseNotes'	=> '<ul><li>Test Update</li><ul>'
+			'branchName'	=> '2.0.1Update',
+			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Adds check for header redirect</li><li>Fixed Unpause - on focus (if default paused) Bug</li><li>Adds remove directory / watch folder button<li></ul></li><ul>'
 		)
 	)
 );
