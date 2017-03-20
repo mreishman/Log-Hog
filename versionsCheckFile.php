9a941c1266ceb8fa2223905c042b2d08f68ab356
Matt Reishman <mreishman@users.noreply.github.com>
2017-03-20 13:12:50 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -3,13 +3,13 @@
 $versionCheckArray = array(
 	'version'		=> '2.0.2',
 	'versionList'		=> array(
-		'2.0.2'	        => array(
-			'branchName'	=> '2.0.2Update',
-			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Renamed titles for settings pages</li><li>Moved changelog info from update page to seperate php file.</li><li>File extractor now extracts files other than php, includes:<ul><li>.css</li><li>.html</li><li>.js</li><li>.png</li><li>.jpg</li><li>.jpeg</li></ul></li><li>Moved var loading part in update scripts to seperate file. Helps updating vars in future.</li></ul></li></ul>'
-		),
 		'2.0.1'	        => array(
 			'branchName'	=> '2.0.1Update',
 			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Adds check for header redirect</li><li>Fixed Unpause - on focus (if default paused) Bug</li><li>Adds remove directory / watch folder button</li></ul></li></ul>'
+		),
+		'2.0.2'	        => array(
+			'branchName'	=> '2.0.2Update',
+			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Renamed titles for settings pages</li><li>Moved changelog info from update page to seperate php file.</li><li>File extractor now extracts files other than php, includes:<ul><li>.css</li><li>.html</li><li>.js</li><li>.png</li><li>.jpg</li><li>.jpeg</li></ul></li><li>Moved var loading part in update scripts to seperate file. Helps updating vars in future.</li></ul></li></ul>'
 		)
 	)
 );
