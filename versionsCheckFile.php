8597268c34d3326ad77e0981c71b4efebbcdef3c
Matt Reishman <mreishman@users.noreply.github.com>
2017-03-20 12:48:59 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -9,9 +9,7 @@ $versionCheckArray = array(
 		),
 		'2.0.2'	        => array(
 			'branchName'	=> '2.0.2Update',
-			'releaseNotes'	=> '<ul><li>
-			2.0.2
-			<ul><li>Renamed titles for settings pages</li><li>Moved changelog info from update page to seperate php file.</li><li>File extractor now extracts files other than php, includes:<ul><li>.css</li><li>.html</li><li>.js</li><li>.png</li><li>.jpg</li><li>.jpeg</li></ul></li><li>Moved var loading part in update scripts to seperate file. Helps updating vars in future.</li></ul></li><ul>'
+			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Renamed titles for settings pages</li><li>Moved changelog info from update page to seperate php file.</li><li>File extractor now extracts files other than php, includes:<ul><li>.css</li><li>.html</li><li>.js</li><li>.png</li><li>.jpg</li><li>.jpeg</li></ul></li><li>Moved var loading part in update scripts to seperate file. Helps updating vars in future.</li></ul></li><ul>'
 		)
 	)
 );
