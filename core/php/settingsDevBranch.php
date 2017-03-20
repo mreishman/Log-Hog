92187cccbbf209b9c8b5a6b7b554ab4ed99762d1
Matt Reishman <matt.reishman@Me.com>
2017-03-20 12:28:55 -0500

    moved load vars to seperate file. Fixes #36
    Moved load vars from update files to a seperate file, easier for adding
    new vars later on (sorta)

--- core/php/settingsDevBranch.php
+++ core/php/settingsDevBranch.php
@@ -11,63 +11,7 @@ if(file_exists('../../local/layout.php'))
 
 require_once($baseUrl.'conf/config.php');
 require_once('../../core/conf/config.php'); 
-	
-if(array_key_exists('watchList', $config))
-{
-	$watchList = $config['watchList'];
-}
-else
-{
-	$watchList = $defaultConfig['watchList'];
-}
-	if(array_key_exists('sliceSize', $config))
-{
-	$sliceSize = $config['sliceSize'];
-}
-else
-{
-	$sliceSize = $defaultConfig['sliceSize'];
-} 
-if(array_key_exists('pollingRate', $config))
-{
-	$pollingRate = $config['pollingRate'];
-}
-else
-{
-	$pollingRate = $defaultConfig['pollingRate'];
-} 
-if(array_key_exists('pausePoll', $config))
-{
-	$pausePoll = $config['pausePoll'];
-}
-else
-{
-	$pausePoll = $defaultConfig['pausePoll'];
-}
-if(array_key_exists('pauseOnNotFocus', $config))
-{
-	$pauseOnNotFocus = $config['pauseOnNotFocus'];
-}
-else
-{
-	$pauseOnNotFocus = $defaultConfig['pauseOnNotFocus'];
-}
-if(array_key_exists('autoCheckUpdate', $config))
-{
-	$autoCheckUpdate = $config['autoCheckUpdate'];
-}
-else
-{
-	$autoCheckUpdate = $defaultConfig['autoCheckUpdate'];
-}
-if(array_key_exists('developmentTabEnabled', $config))
-{
-	$developmentTabEnabled = $config['developmentTabEnabled'];
-}
-else
-{
-	$developmentTabEnabled = $defaultConfig['developmentTabEnabled'];
-}
+require_once('loadVars.php');
 
 
 
--- core/php/settingsMainUpdateVars.php
+++ core/php/settingsMainUpdateVars.php
@@ -11,45 +11,7 @@ if(file_exists('../../local/layout.php'))
 
 require_once($baseUrl.'conf/config.php');
 require_once('../../core/conf/config.php'); 
-	
-	if(array_key_exists('watchList', $config))
-	{
-		$watchList = $config['watchList'];
-	}
-	else
-	{
-		$watchList = $defaultConfig['watchList'];
-	}
-	if(array_key_exists('developmentTabEnabled', $config))
-	{
-		$developmentTabEnabled = $config['developmentTabEnabled'];
-	}
-	else
-	{
-		$developmentTabEnabled = $defaultConfig['developmentTabEnabled'];
-	}
-	if(array_key_exists('enableDevBranchDownload', $config))
-	{
-		$enableDevBranchDownload = $config['enableDevBranchDownload'];
-	}
-	else
-	{
-		$enableDevBranchDownload = $defaultConfig['enableDevBranchDownload'];
-	}
-
-
-	$arrayWatchList = "";
-	$numberOfRows = count($watchList);
-	$i = 0;
-	foreach ($watchList as $key => $value) 
-	{
-		$i++;
-		$arrayWatchList .= "'".$key."' => '".$value."'";
-		if($i != $numberOfRows)
-		{
-			$arrayWatchList .= ",";
-		}
-	}
+require_once('loadVars.php');
 
 	$fileName = ''.$baseUrl.'conf/config.php';
 
