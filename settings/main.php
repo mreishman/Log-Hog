b05dd8313ef662736765e182a8bf8acd7ff124aa
Matt Reishman <matt.reishman@Me.com>
2017-04-24 08:49:01 -0500

    Adds file permissions to file view. Fixes #60

--- settings/main.php
+++ settings/main.php
@@ -167,6 +167,60 @@ require_once('../core/php/loadVars.php');
 				$i = 0;
 				$triggerSaveUpdate = false;
 				foreach($config['watchList'] as $key => $item): $i++;
+
+				$perms  =  fileperms($key); 
+
+				switch ($perms & 0xF000) {
+				    case 0xC000: // socket
+				        $info = 's';
+				        break;
+				    case 0xA000: // symbolic link
+				        $info = 'l';
+				        break;
+				    case 0x8000: // regular
+				        $info = 'r';
+				        break;
+				    case 0x6000: // block special
+				        $info = 'b';
+				        break;
+				    case 0x4000: // directory
+				        $info = 'd';
+				        break;
+				    case 0x2000: // character special
+				        $info = 'c';
+				        break;
+				    case 0x1000: // FIFO pipe
+				        $info = 'p';
+				        break;
+				    default: // unknown
+				        $info = 'u';
+				}
+
+				// Owner
+				$info .= (($perms & 0x0100) ? 'r' : '-');
+				$info .= (($perms & 0x0080) ? 'w' : '-');
+				$info .= (($perms & 0x0040) ?
+				            (($perms & 0x0800) ? 's' : 'x' ) :
+				            (($perms & 0x0800) ? 'S' : '-'));
+
+				// Group
+				$info .= (($perms & 0x0020) ? 'r' : '-');
+				$info .= (($perms & 0x0010) ? 'w' : '-');
+				$info .= (($perms & 0x0008) ?
+				            (($perms & 0x0400) ? 's' : 'x' ) :
+				            (($perms & 0x0400) ? 'S' : '-'));
+
+				// World
+				$info .= (($perms & 0x0004) ? 'r' : '-');
+				$info .= (($perms & 0x0002) ? 'w' : '-');
+				$info .= (($perms & 0x0001) ?
+				            (($perms & 0x0200) ? 't' : 'x' ) :
+				            (($perms & 0x0200) ? 'T' : '-'));
+
+
+
+
+
 				if(strpos($item, "\\") !== false)
 				{
 					$item = str_replace("\\", "", $item);
@@ -174,7 +228,7 @@ require_once('../core/php/loadVars.php');
 				}
 				?>
 			<li id="rowNumber<?php echo $i; ?>" >
-				File #<?php if($i < 10){echo "0";} ?><?php echo $i; ?>:
+				File #<?php if($i < 10){echo "0";} ?><?php echo $i; ?>: &nbsp; <?php echo $info; ?> &nbsp;
 				<?php
 				if(!file_exists($key))
 				{
