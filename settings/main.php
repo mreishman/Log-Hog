c6b18febb231efef237302b30e1eb46987261545
Matt Reishman <matt.reishman@Me.com>
2017-05-22 13:21:14 -0500

    Added to key, also doesn't try to get file perms if no file.

--- settings/main.php
+++ settings/main.php
@@ -252,59 +252,61 @@ require_once('../core/php/loadVars.php');
 				$i = 0;
 				$triggerSaveUpdate = false;
 				foreach($config['watchList'] as $key => $item): $i++;
+				if(file_exists($key))
+				{
+					$perms  =  fileperms($key); 
 
-				$perms  =  fileperms($key); 
+					switch ($perms & 0xF000) {
+					    case 0xC000: // socket
+					        $info = 's';
+					        break;
+					    case 0xA000: // symbolic link
+					        $info = 'l';
+					        break;
+					    case 0x8000: // regular
+					        $info = 'f';
+					        break;
+					    case 0x6000: // block special
+					        $info = 'b';
+					        break;
+					    case 0x4000: // directory
+					        $info = 'd';
+					        break;
+					    case 0x2000: // character special
+					        $info = 'c';
+					        break;
+					    case 0x1000: // FIFO pipe
+					        $info = 'p';
+					        break;
+					    default: // unknown
+					        $info = 'u';
+					}
 
-				switch ($perms & 0xF000) {
-				    case 0xC000: // socket
-				        $info = 's';
-				        break;
-				    case 0xA000: // symbolic link
-				        $info = 'l';
-				        break;
-				    case 0x8000: // regular
-				        $info = 'r';
-				        break;
-				    case 0x6000: // block special
-				        $info = 'b';
-				        break;
-				    case 0x4000: // directory
-				        $info = 'd';
-				        break;
-				    case 0x2000: // character special
-				        $info = 'c';
-				        break;
-				    case 0x1000: // FIFO pipe
-				        $info = 'p';
-				        break;
-				    default: // unknown
-				        $info = 'u';
+					// Owner
+					$info .= (($perms & 0x0100) ? 'r' : '-');
+					$info .= (($perms & 0x0080) ? 'w' : '-');
+					$info .= (($perms & 0x0040) ?
+					            (($perms & 0x0800) ? 's' : 'x' ) :
+					            (($perms & 0x0800) ? 'S' : '-'));
+
+					// Group
+					$info .= (($perms & 0x0020) ? 'r' : '-');
+					$info .= (($perms & 0x0010) ? 'w' : '-');
+					$info .= (($perms & 0x0008) ?
+					            (($perms & 0x0400) ? 's' : 'x' ) :
+					            (($perms & 0x0400) ? 'S' : '-'));
+
+					// World
+					$info .= (($perms & 0x0004) ? 'r' : '-');
+					$info .= (($perms & 0x0002) ? 'w' : '-');
+					$info .= (($perms & 0x0001) ?
+					            (($perms & 0x0200) ? 't' : 'x' ) :
+					            (($perms & 0x0200) ? 'T' : '-'));
 				}
-
-				// Owner
-				$info .= (($perms & 0x0100) ? 'r' : '-');
-				$info .= (($perms & 0x0080) ? 'w' : '-');
-				$info .= (($perms & 0x0040) ?
-				            (($perms & 0x0800) ? 's' : 'x' ) :
-				            (($perms & 0x0800) ? 'S' : '-'));
-
-				// Group
-				$info .= (($perms & 0x0020) ? 'r' : '-');
-				$info .= (($perms & 0x0010) ? 'w' : '-');
-				$info .= (($perms & 0x0008) ?
-				            (($perms & 0x0400) ? 's' : 'x' ) :
-				            (($perms & 0x0400) ? 'S' : '-'));
-
-				// World
-				$info .= (($perms & 0x0004) ? 'r' : '-');
-				$info .= (($perms & 0x0002) ? 'w' : '-');
-				$info .= (($perms & 0x0001) ?
-				            (($perms & 0x0200) ? 't' : 'x' ) :
-				            (($perms & 0x0200) ? 'T' : '-'));
-
-
-
-
+				else
+				{
+					$info = "u---------";
+				}
 
 				if(strpos($item, "\\") !== false)
 				{
@@ -342,6 +344,14 @@ require_once('../core/php/loadVars.php');
 				<ul id="settingsUl">
 					<li>
 						<img src="../core/img/redWarning.png" height="10px"> - File / Folder not found!
+					</li>
+					<li>
+						f - file &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp;
+						d - directory &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp;
+						u - unknown / file not found &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp;
+						r - readable &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp;
+						w - writeable &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp;
+						x - executable
 					</li>
 				</ul>
 			</li>
