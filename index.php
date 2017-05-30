cbe426a76a5815a96ea6a721061e6a256cc9c119
Matt Reishman <mreishman@users.noreply.github.com>
2017-05-30 08:51:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -146,7 +146,7 @@ if($pollingRateType == 'Seconds')
 	</div>
 	
 	<div id="titleContainer"><div id="title">&nbsp;</div>&nbsp;&nbsp;<form style="display: inline-block;" ><a class="linkSmall" onclick="clearLog()" >Clear Log</a><a class="linkSmall" onclick="deleteLogPopup()" >Delete Log</a></form></div>
-	<form id="settingsInstallUpdate" action="../update/updater.php" method="post" style="display: none"></form>
+	<form id="settingsInstallUpdate" action="update/updater.php" method="post" style="display: none"></form>
 	<script>
 		var Rightclick_ID_list = [];
 		if(document.getElementById('gear'))
