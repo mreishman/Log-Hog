cbc1af04df393d9e2f5ca7352f91281f5c65b8a4
Matt Reishman <mreishman@users.noreply.github.com>
2017-07-11 10:28:55 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -1,7 +1,7 @@
 <?php
 
 $versionCheckArray = array(
-	'version'		=> '2.3.1',
+	'version'		=> '2.3.2',
 	'versionList'		=> array(
 		'2.0.1'	        => array(
 			'branchName'	=> '2.0.1Update',
@@ -34,6 +34,10 @@ $versionCheckArray = array(
 		'2.3.1'		=> array(
 			'branchName'	=> '2.3.1Update',
 			'releaseNotes'	=> '<ul><li>Features<ul><li>New poll logic added (~ 25% improvement)</li></ul></li><li>Bug Fixes<ul><li>Fixed issue with ram not showing correct information on some servers</li><li>Fixed issue where clear / delete buttons didn not work properly. </li></ul></li></ul>'
+		),
+		'2.3.2'		=> array(
+			'branchName'	=> '2.3.2Update',
+			'releaseNotes'	=> '<ul><li>Features<ul><li>Added setup to first launch</li><li>Added version check to poll logic, popup if version changed</li></ul></li><li>Bug Fixes<ul><li>Fixed compatability issues with php 7</li><li>Updated poll logic again, should be slightly faster.</li><li>Added buffer to trim logic, less trim calls made</li></ul></li></ul>'
 		)
 	)
 );
