a65fa1d3ac81e3146951cabf3e9e04fb2ad7fb6e
Matt Reishman <matt.reishman@me.com>
2017-07-22 10:02:01 -0500

    Added max retries to check. Added popup Fixes #127

--- setup/stepsJavascript.js
+++ setup/stepsJavascript.js
@@ -1,4 +1,5 @@
 var pollCheckForUpdate;
+var countChecker = 0;
 
 function updateStatus(status)
 {
@@ -19,27 +20,38 @@ function updateStatus(status)
 
 function verifyStatusChange(status)
 {
-	var urlForSend = './updateSetupCheck.php?format=json'
-	var data = {status: status };
-	$.ajax({
-			  url: urlForSend,
-			  dataType: 'json',
-			  data: data,
-			  type: 'POST',
-	success: function(data)
+	countChecker++;
+	if(countChecker < 10)
+	{
+		var urlForSend = './updateSetupCheck.php?format=json'
+		var data = {status: status };
+		$.ajax(
+		{
+			url: urlForSend,
+			dataType: 'json',
+			data: data,
+			type: 'POST',
+			success: function(data)
+			{
+				clearInterval(pollCheckForUpdate);
+				if(data == true)
+				{
+					if(status == "finished")
+					{
+						defaultSettings();
+					}
+					else
+					{
+						customSettings();
+					}
+				}
+		  	},
+		});
+	}
+	else
 	{
 		clearInterval(pollCheckForUpdate);
-		if(data == true)
-		{
-			if(status == "finished")
-			{
-				defaultSettings();
-			}
-			else
-			{
-				customSettings();
-			}
-		}
-  	},
-		});
+		showPopup();
+		document.getElementById('popupContentInnerHTMLDiv').innerHTML = "<div class='settingsHeader' >An error occured?</div><br><div style='width:100%;text-align:center;padding-left:10px;padding-right:10px;'>An error occured while trying to save settings. Try again?</div><div class='link' onclick='window.location.href = "+'"../"'+";' style='margin-left:125px; margin-right:50px;margin-top:25px;'>No</div><div onclick='hidePopup();' class='link'>Yes</div></div>";
+	}
 }
