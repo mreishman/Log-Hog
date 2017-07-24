b6b461d3576a4e21d156a20f6d992aca1a6d44d3
Matt Reishman <matt.reishman@Me.com>
2017-07-24 16:24:26 -0500

    Added missing simicolons

--- core/php/resetUpdateFilesToDefault.php
+++ core/php/resetUpdateFilesToDefault.php
@@ -1,29 +1,29 @@
 <?php
 
-$fileName = "updateProgressFileNext.php"
+$fileName = "updateProgressFileNext.php";
 $newInfoForConfig = "<?php
 	$"."updateProgress = array(
   	'currentStep'   => 'Finished Updating to ',
   	'action' => 'finishedUpdate'
 	);
-	?>"
+	?>";
 file_put_contents($fileName, $newInfoForConfig);
 
-$fileName = "updateProgressFile.php"
+$fileName = "updateProgressFile.php";
 $newInfoForConfig = "<?php
 	$"."updateProgress = array(
   	'currentStep'   => 'Finished Updating to ',
   	'action' => 'finishedUpdate'
 	);
-	?>"
+	?>";
 file_put_contents($fileName, $newInfoForConfig);
 
-$fileName = "updateProgressLog.php"
-$newInfoForConfig = "<p> Loading update file list. </p>"
+$fileName = "updateProgressLog.php";
+$newInfoForConfig = "<p> Loading update file list. </p>";
 file_put_contents($fileName, $newInfoForConfig);
 
-$fileName = "updateProgressLogHead.php"
-$newInfoForConfig = "<p id='headerForUpdate'><?php echo $"."updateProgress['currentStep']; echo ' '; echo $"."versionToUpdate; ?>.</p>"
+$fileName = "updateProgressLogHead.php";
+$newInfoForConfig = "<p id='headerForUpdate'><?php echo $"."updateProgress['currentStep']; echo ' '; echo $"."versionToUpdate; ?>.</p>";
 file_put_contents($fileName, $newInfoForConfig);
 
 echo json_encode(true);
