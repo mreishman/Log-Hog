088d607c0971dcc07bb5cec402d360434e3cbcd9
Matt Reishman <matt.reishman@Me.com>
2017-07-24 12:35:22 -0500

    fixed issue with using same name for vars

--- core/php/loadVars.php
+++ core/php/loadVars.php
@@ -60,117 +60,120 @@ foreach ($folderColorArrays as $key => $value)
 	}
 }
 
-$arrayWatchList = "";
-if(isset($_POST['numberOfRows']))
+if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-	for($i = 1; $i <= $_POST['numberOfRows']; $i++ )
+	$arrayWatchList = "";
+	if(isset($_POST['numberOfRows']))
 	{
-		$arrayWatchList .= "'".$_POST['watchListKey'.$i]."' => '".$_POST['watchListItem'.$i]."'";
-		if($i != $_POST['numberOfRows'])
+		for($i = 1; $i <= $_POST['numberOfRows']; $i++ )
 		{
-			$arrayWatchList .= ",";
-		}
-	}
-}
-else
-{
-	$numberOfRows = count($watchList);
-	$i = 0;
-	foreach ($watchList as $key => $value) 
-	{
-		$i++;
-		$arrayWatchList .= "'".$key."' => '".$value."'";
-		if($i != $numberOfRows)
-		{
-			$arrayWatchList .= ",";
-		}
-	}
-}
-$watchList = $arrayWatchList;
-
-$popupSettingsArraySave = "";
-if($popupWarnings == "all")
-{
-	$popupSettingsArraySave = "
-		'saveSettings'	=>	'true',
-		'blankFolder'	=>	'true',
-		'deleteLog'	=>	'true',
-		'removeFolder'	=> 	'true',
-		'versionCheck'	=> 'true'
-		";
-}
-elseif($popupWarnings == "none")
-{
-	$popupSettingsArraySave = "
-		'saveSettings'	=>	'false',
-		'blankFolder'	=>	'false',
-		'deleteLog'	=>	'false',
-		'removeFolder'	=> 	'false',
-		'versionCheck'	=> 'false'
-		";
-}
-else
-{
-	if(isset($_POST['saveSettings']))
-	{
-		$popupSettingsArraySave = "
-		'saveSettings'	=>	'".$_POST['saveSettings']."',
-		'blankFolder'	=>	'".$_POST['blankFolder']."',
-		'deleteLog'	=>	'".$_POST['deleteLog']."',
-		'removeFolder'	=> 	'".$_POST['removeFolder']."',
-		'versionCheck'	=> '".$_POST['versionCheck']."'
-		";
-	}
-	else
-	{
-		$popupSettingsArraySave = "";
-		$count = 0;
-		foreach ($popupSettingsArray as $key => $value)
-		{
-			$popupSettingsArraySave .= "'".$key."'	=>	'".$value."'";
-			$count++;
-			if($count != 4)
+			$arrayWatchList .= "'".$_POST['watchListKey'.$i]."' => '".$_POST['watchListItem'.$i]."'";
+			if($i != $_POST['numberOfRows'])
 			{
-				$popupSettingsArraySave .= ",";
+				$arrayWatchList .= ",";
 			}
 		}
 	}
-}
-$popupSettingsArray = $popupSettingsArraySave;
+	else
+	{
+		$numberOfRows = count($watchList);
+		$i = 0;
+		foreach ($watchList as $key => $value) 
+		{
+			$i++;
+			$arrayWatchList .= "'".$key."' => '".$value."'";
+			if($i != $numberOfRows)
+			{
+				$arrayWatchList .= ",";
+			}
+		}
+	}
+	$watchList = $arrayWatchList;
 
-$folderColorArraysSave = "";
-if(isset($_POST['folderThemeCount']))
-{
-	$intFolderThemeCount = intval($_POST['folderThemeCount']);
-	for($i = 0; $i < $intFolderThemeCount; $i++ )
+	$popupSettingsArraySave = "";
+	if($popupWarnings == "all")
 	{
-		$folderColorArraysSave .= "'".$_POST['folderColorThemeNameForPost'.($i+1)]."'	=>	array(";
-		$colorCount = 0;
-		while (isset($_POST['folderColorValue'.($i+1).'-'.($colorCount+1)])) 
-		{
-			$colorCount++;
-			$folderColorArraysSave .= "'".$_POST['folderColorValue'.($i+1).'-'.($colorCount)]."',";
-		}
-		$folderColorArraysSave = substr($folderColorArraysSave, 0, -1);
-		$folderColorArraysSave .= ")";
-		$folderColorArraysSave .= ",";
+		$popupSettingsArraySave = "
+			'saveSettings'	=>	'true',
+			'blankFolder'	=>	'true',
+			'deleteLog'	=>	'true',
+			'removeFolder'	=> 	'true',
+			'versionCheck'	=> 'true'
+			";
 	}
-}
-else
-{
-	$count = 0;
-	foreach ($folderColorArrays as $key => $value)
+	elseif($popupWarnings == "none")
 	{
-		$folderColorArraysSave .= "'".$key."'	=>	array(";
-		$count++;
-		foreach ($value as $key2 => $value2) 
-		{
-			$folderColorArraysSave .= "'".$value2."',";
-		}
-		$folderColorArraysSave = substr($folderColorArraysSave, 0, -1);
-		$folderColorArraysSave .= ")";
-		$folderColorArraysSave .= ",";
+		$popupSettingsArraySave = "
+			'saveSettings'	=>	'false',
+			'blankFolder'	=>	'false',
+			'deleteLog'	=>	'false',
+			'removeFolder'	=> 	'false',
+			'versionCheck'	=> 'false'
+			";
 	}
+	else
+	{
+		if(isset($_POST['saveSettings']))
+		{
+			$popupSettingsArraySave = "
+			'saveSettings'	=>	'".$_POST['saveSettings']."',
+			'blankFolder'	=>	'".$_POST['blankFolder']."',
+			'deleteLog'	=>	'".$_POST['deleteLog']."',
+			'removeFolder'	=> 	'".$_POST['removeFolder']."',
+			'versionCheck'	=> '".$_POST['versionCheck']."'
+			";
+		}
+		else
+		{
+			$popupSettingsArraySave = "";
+			$count = 0;
+			foreach ($popupSettingsArray as $key => $value)
+			{
+				$popupSettingsArraySave .= "'".$key."'	=>	'".$value."'";
+				$count++;
+				if($count != 4)
+				{
+					$popupSettingsArraySave .= ",";
+				}
+			}
+		}
+	}
+	$popupSettingsArray = $popupSettingsArraySave;
+
+	$folderColorArraysSave = "";
+	if(isset($_POST['folderThemeCount']))
+	{
+		$intFolderThemeCount = intval($_POST['folderThemeCount']);
+		for($i = 0; $i < $intFolderThemeCount; $i++ )
+		{
+			$folderColorArraysSave .= "'".$_POST['folderColorThemeNameForPost'.($i+1)]."'	=>	array(";
+			$colorCount = 0;
+			while (isset($_POST['folderColorValue'.($i+1).'-'.($colorCount+1)])) 
+			{
+				$colorCount++;
+				$folderColorArraysSave .= "'".$_POST['folderColorValue'.($i+1).'-'.($colorCount)]."',";
+			}
+			$folderColorArraysSave = substr($folderColorArraysSave, 0, -1);
+			$folderColorArraysSave .= ")";
+			$folderColorArraysSave .= ",";
+		}
+	}
+	else
+	{
+		$count = 0;
+		foreach ($folderColorArrays as $key => $value)
+		{
+			$folderColorArraysSave .= "'".$key."'	=>	array(";
+			$count++;
+			foreach ($value as $key2 => $value2) 
+			{
+				$folderColorArraysSave .= "'".$value2."',";
+			}
+			$folderColorArraysSave = substr($folderColorArraysSave, 0, -1);
+			$folderColorArraysSave .= ")";
+			$folderColorArraysSave .= ",";
+		}
+	}
+	$folderColorArrays = $folderColorArraysSave;
 }
-$folderColorArrays = $folderColorArraysSave;
 ?>
