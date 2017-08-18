b4f68a35ec5d8bf961ab90d385ce23ef32a9c4e4
Matt Reishman <mreishman@users.noreply.github.com>
2017-08-18 15:21:45 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -50,11 +50,7 @@ $versionCheckArray = array(
 		'2.3.5'		=> array(
 			'branchName'	=> '2.3.5Update',
 			'releaseNotes'	=> "<ul><li>Features<ul><li>Reduced poll times by an average of 20ms</li><li>Added advanced poll options</li><li>Updated updater, now with more ajax!</li></ul></li><li>Bug Fixes<ul><li>Saving now does not refresh page, also adds check if save worked</li><li>Checking for update checks if update information was saved</li><li>fixed style issues with process table in monitor</li><li>Fixed bug with poll logic if first poll failed</li><li>Fixed bug with pause icon when loading with pause by default</li><li>Fixed bug with pause on focus not always following settings if disabled</li><li>Fixed bug with some forms not resetting correctly</li><li>Fixed bug with some forms giving false positives for changes in form data</li><li>Fixed issues with ajax update check on index page not waiting for file to refresh contents</li><li>Fixed issues with ajax update check on update page not refreshing install button</li><li>Fixed issues with ajax update check on update page sometimes not display proper changelog</li><li>Fixed bug where deleting file then adding file would result in a save error</li></ul></li></ul>"
-		),
-		'2.3.6'		=> array(
-			'branchName'	=> '2.3.6Update',
-			'releaseNotes'	=> "<ul><li>Test</li></ul>"
-		),
+		)
 	)
 );
 ?>
