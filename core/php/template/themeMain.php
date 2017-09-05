a8fcbf214e5ee630215f9ea26f233b423ddc2d70
Matt Reishman <matt.reishman@Me.com>
2017-09-05 12:14:17 -0500

    More changes to default template for themes #25

--- core/php/template/themeMain.php
+++ core/php/template/themeMain.php
@@ -1,13 +1,6 @@
 <form id="themeMainVars" action="../core/php/settingsSave.php" method="post">
 <div class="settingsHeader">
 Theme Selector
-<div class="settingsHeaderButtons">
-	<?php if ($setupProcess == "preStart" || $setupProcess == "finished"): ?>
-		<a class="linkSmall" onclick="saveAndVerifyMain('themeMainVars');" >Save Changes</a>
-	<?php else: ?>
-		<button  onclick="displayLoadingPopup();">Save Changes</button>
-	<?php endif; ?>
-</div>
 </div>
 <div class="settingsDiv" >
 	<?php
@@ -16,7 +9,18 @@ Theme Selector
 	foreach ($scanned_directory as $key): ?>
 		<div style="width: 600px; height: 400px; display: inline-block; background-color: grey; border: 1px solid white; margin: 10px;">
 			<div class="settingsHeader" style="margin: 0px;">
-			<?php echo $key;?>
+				<?php echo $key;?>
+				<div class="settingsHeaderButtons">
+					<?php if(true): ?>
+						<?php if ($setupProcess == "preStart" || $setupProcess == "finished"): ?>
+						<a class="linkSmall" onclick="saveAndVerifyMain('settingsMainVars');" >Select</a>
+						<?php else: ?>
+							<button  onclick="displayLoadingPopup();">Select</button>
+						<?php endif; ?>
+					<?php else: ?>
+						Selected
+					<?php endif;?>
+				</div>
 			</div>
 			<?php echo generateExampleIndex($key, $withLogHog);?>
 		</div>
