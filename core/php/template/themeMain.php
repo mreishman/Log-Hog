ae0da5c19f619ce540536c4c58d538a0de877131
Matt Reishman <matt.reishman@me.com>
2017-09-09 13:56:55 -0500

    Removed .DS_Store if in themes folder from showing up.

--- core/php/template/themeMain.php
+++ core/php/template/themeMain.php
@@ -6,24 +6,27 @@ Theme Selector
 	<?php
 	$directory = '../core/Themes/';
 	$scanned_directory = array_diff(scandir($directory), array('..', '.'));
-	foreach ($scanned_directory as $key): ?>
-		<div style="width: 600px; height: 400px; display: inline-block; background-color: grey; border: 1px solid white; margin: 20px;">
-			<div class="settingsHeader" style="margin: 0px;">
-				<?php echo $key;?>
-				<div class="settingsHeaderButtons">
-					<?php if($key !== $currentTheme): ?>
-						<?php if ($setupProcess == "preStart" || $setupProcess == "finished"): ?>
-						<a class="linkSmall" onclick="saveAndVerifyMain('settingsMainVars');" >Select</a>
+	foreach ($scanned_directory as $key): 
+		if($key != ".DS_Store"):
+		?>
+			<div style="width: 600px; height: 400px; display: inline-block; background-color: grey; border: 1px solid white; margin: 20px;">
+				<div class="settingsHeader" style="margin: 0px;">
+					<?php echo $key;?>
+					<div class="settingsHeaderButtons">
+						<?php if($key !== $currentTheme): ?>
+							<?php if ($setupProcess == "preStart" || $setupProcess == "finished"): ?>
+							<a class="linkSmall" onclick="saveAndVerifyMain('settingsMainVars');" >Select</a>
+							<?php else: ?>
+								<button  onclick="displayLoadingPopup();">Select</button>
+							<?php endif; ?>
 						<?php else: ?>
-							<button  onclick="displayLoadingPopup();">Select</button>
-						<?php endif; ?>
-					<?php else: ?>
-						<a class="linkSmallHover"> Selected </a>
-					<?php endif;?>
+							<a class="linkSmallHover"> Selected </a>
+						<?php endif;?>
+					</div>
 				</div>
+				<?php echo generateExampleIndex($key, $withLogHog);?>
 			</div>
-			<?php echo generateExampleIndex($key, $withLogHog);?>
-		</div>
-	<?php endforeach; ?>
+		<?php endif; 
+	endforeach; ?>
 </div>
 </form>
