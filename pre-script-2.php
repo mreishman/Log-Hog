91fa7bf04da0c52d1317eaac41c79b7291b27496
Matt Reishman <mreishman@users.noreply.github.com>
2017-09-27 08:48:54 -0500

    Update pre-script-2.php

--- pre-script-2.php
+++ pre-script-2.php
@@ -2,14 +2,14 @@
 
 sleep(3);
 
-if (!file_exists("../../../../local/Default/img/")) 
+if (!file_exists("../../../../local/default/img/")) 
 {
-	mkdir("../../../../local/Default/img/", 0777, true);
+	mkdir("../../../../local/default/img/", 0777, true);
 }
 
 sleep(3);
 
-echo json_encode("../../../../local/Default/img/");
+echo json_encode("../../../../local/default/img/");
 
 //TRUE for no check, or filename / path for check
 ?>
