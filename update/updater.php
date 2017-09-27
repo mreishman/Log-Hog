f096846626af6c16342dffa44f1d156c84218d5a
Matt Reishman <matt.reishman@Me.com>
2017-09-27 10:17:17 -0500

    More updater bug fixes

--- update/updater.php
+++ update/updater.php
@@ -604,13 +604,13 @@ if(count($arrayOfVersions) === 0)
 		if(preScriptCount != 1)
 		{
 			var totalCount = 1;
-			var fileName = "pre-script-"+totalCount;
-			var loop = ($.inArray(fileName,arrayOfFilesExtracted)!= "-1");
+			var fileName = "pre-script-"+totalCount+".php";
+			var loop = ($.inArray(fileName,arrayOfFilesExtracted)!== -1);
 			while(loop)
 			{
 				totalCount++;
 				fileName = "pre-script-"+totalCount;
-				loop = ($.inArray(fileName,arrayOfFilesExtracted)!= "-1");
+				loop = ($.inArray(fileName,arrayOfFilesExtracted)!== -1);
 			}
 			updateProgressBar(((1/totalCount)*5));
 		}
@@ -745,13 +745,13 @@ if(count($arrayOfVersions) === 0)
 		if(postScriptCount != 1)
 		{
 			var totalCount = 1;
-			var fileName = "post-script-"+totalCount;
-			var loop = ($.inArray(fileName,arrayOfFilesExtracted)!= "-1");
+			var fileName = "post-script-"+totalCount+".php";
+			var loop = ($.inArray(fileName,arrayOfFilesExtracted)!== -1);
 			while(loop)
 			{
 				totalCount++;
 				fileName = "post-script-"+totalCount;
-				loop = ($.inArray(fileName,arrayOfFilesExtracted)!= "-1");
+				loop = ($.inArray(fileName,arrayOfFilesExtracted)!== -1);
 			}
 			updateProgressBar(((1/totalCount)*5));
 		}
