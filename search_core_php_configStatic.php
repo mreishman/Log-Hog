83d5413f7b9156452ae253d47a4600c18e1950f8
Matt Reishman <mreishman@users.noreply.github.com>
2017-10-23 19:35:22 -0500

    Add files via upload

---
+++ search_core_php_configStatic.php
@@ -0,0 +1,10 @@
+<?php
+
+$configStatic = array(
+  'version'   => '1.0',
+  'lastCheck'   => '10-23-2017',
+  'newestVersion' => '1.0',
+  'versionList' => array(
+  '1.0' => array('branchName' => '1.0','releaseNotes' => '<ul><li>Initial Release</li></ul>')
+  )
+);
