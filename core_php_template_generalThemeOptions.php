465762b96ea3a4361429ad012cc0cf1364ba141b
Matt Reishman <mreishman@users.noreply.github.com>
2017-10-23 19:25:05 -0500

    Add files via upload

---
+++ core_php_template_generalThemeOptions.php
@@ -0,0 +1,37 @@
+<form id="settingsColorFolderVars" action="../core/php/settingsSave.php" method="post">
+	<div class="settingsHeader">
+	Main Theme Options
+	<div class="settingsHeaderButtons">
+		<?php if ($setupProcess == "preStart" || $setupProcess == "finished"): ?>
+		<a class="linkSmall" onclick="saveAndVerifyMain('settingsColorFolderVars');" >Save Changes</a>
+		<?php else: ?>
+			<button  onclick="displayLoadingPopup();">Save Changes</button>
+		<?php endif; ?>
+	</div>
+	</div>
+	<div class="settingsDiv" >
+		<ul id="settingsUl">
+			<li>
+				<span class="settingsBuffer" > Background: </span> 
+				<input type="text" name="backgroundColor" value="<?php echo $backgroundColor;?>" >
+			</li>
+			<li>
+				<span class="settingsBuffer" > Main Font Color: </span> 
+				<input type="text" name="mainFontColor" value="<?php echo $mainFontColor;?>" >
+			</li>
+			<li> 
+				<span class="settingsBuffer" > Header Background: </span> 
+				<input type="text" name="backgroundHeaderColor" value="<?php echo $backgroundHeaderColor;?>" >
+			</li>
+			<li>
+				<span class="settingsBuffer"> Invert Header Images: </span>
+				<div class="selectDiv">
+					<select name="invertMenuImages">
+						<option <?php if($invertMenuImages === 'true'){echo "selected";} ?> value="true">True</option>
+						<option <?php if($invertMenuImages === 'false'){echo "selected";} ?> value="false">False</option>
+					</select>
+				</div>
+			</li>
+		</ul>
+	</div>
+</form>
