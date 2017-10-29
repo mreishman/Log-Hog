4acd7ed35d6ff198632b8e5862f63afaeff472d9
Matt Reishman <matt.reishman@me.com>
2017-10-29 16:27:40 -0500

    Fixed bug with popup bool var not  for all outputs #225

--- core/js/update.js
+++ core/js/update.js
@@ -1,7 +1,7 @@
 var urlSend = "";
 var whatAmIUpdating = "";
 var updateFormID = "settingsInstallUpdate";
-var showPopupForUpdate = true;
+var showPopupForUpdateBool = true;
 var dontNotifyVersionNotSet = "";
 var dataFromJSON = "";
 
@@ -11,9 +11,9 @@ function checkForUpdates(urlSend = "../", whatAmIUpdating = "Log-Hog", currentNe
 	urlSend = urlSend;
 	whatAmIUpdating = whatAmIUpdating;
 	updateFormID = updateFormIDLocal;
-	showPopupForUpdate = showPopupForUpdateInner;
+	showPopupForUpdateBool = showPopupForUpdateInner;
 	dontNotifyVersionNotSet = dontNotifyVersionInner;
-	if(showPopupForUpdate)
+	if(showPopupForUpdateBool)
 	{
 		displayLoadingPopup();
 	}
@@ -29,11 +29,17 @@ function checkForUpdates(urlSend = "../", whatAmIUpdating = "Log-Hog", currentNe
 		}
 		else if (data.version == "0")
 		{
-			document.getElementById("popupContentInnerHTMLDiv").innerHTML = "<div class='settingsHeader' >No Update For "+whatAmIUpdating+" </div><br><div style='width:100%;text-align:center;padding-left:10px;padding-right:10px;'>You are on the most current version</div><div class='link' onclick='closePopupNoUpdate();' style='margin-left:165px; margin-right:50px;margin-top:25px;'>Okay!</div></div>";
+			if(showPopupForUpdateBool)
+			{
+				document.getElementById("popupContentInnerHTMLDiv").innerHTML = "<div class='settingsHeader' >No Update For "+whatAmIUpdating+" </div><br><div style='width:100%;text-align:center;padding-left:10px;padding-right:10px;'>You are on the most current version</div><div class='link' onclick='closePopupNoUpdate();' style='margin-left:165px; margin-right:50px;margin-top:25px;'>Okay!</div></div>";
+			}
 		}
 		else
 		{
-			document.getElementById("popupContentInnerHTMLDiv").innerHTML = "<div class='settingsHeader' >Error</div><div style='width:100%;text-align:center;padding-left:10px;padding-right:10px;'>An error occured while trying to check for updates for "+whatAmIUpdating+". Make sure you are connected to the internet and settingsCheckForUpdate.php has sufficient rights to write / create files. </div><div class='link' onclick='closePopupNoUpdate();' style='margin-left:165px; margin-right:50px;margin-top:5px;'>Okay!</div></div>";
+			if(showPopupForUpdateBool)
+			{
+				document.getElementById("popupContentInnerHTMLDiv").innerHTML = "<div class='settingsHeader' >Error</div><div style='width:100%;text-align:center;padding-left:10px;padding-right:10px;'>An error occured while trying to check for updates for "+whatAmIUpdating+". Make sure you are connected to the internet and settingsCheckForUpdate.php has sufficient rights to write / create files. </div><div class='link' onclick='closePopupNoUpdate();' style='margin-left:165px; margin-right:50px;margin-top:5px;'>Okay!</div></div>";
+			}
 		}
 		
 	});
@@ -116,7 +122,7 @@ function saveSettingFromPopupNoCheckMaybe()
 			$.ajax({
 				url: urlForSend,
 				dataType: "json",
-				data: data,
+				data,
 				type: "POST",
 			complete(data){
 				closePopupNoUpdate();
