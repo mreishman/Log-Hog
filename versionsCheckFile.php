3697500e80aace9cd4d2566896f07fc6c9f344ef
Matt Reishman <mreishman@users.noreply.github.com>
2017-12-04 08:50:30 -0600

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -1,7 +1,7 @@
 <?php
 
 $versionCheckArray = array(
-	'version'		=> '3.2',
+	'version'		=> '3.2.1',
 	'versionList'		=> array(
 		'2.0.1'	        => array(
 			'branchName'	=> '2.0.1Update',
@@ -94,6 +94,10 @@ $versionCheckArray = array(
 		'3.2'		=> array(
 			'branchName'	=> '3.2Update',
 			'releaseNotes'	=> "<ul><li>Features<ul><li>Addons page (page for addons)</li><li>Added new font options</li><li>Selenium monitor addon</li><li>Config restore options</li></ul></li><li>Bug Fixes<ul><li>Fixed delay when resetting update notification</li><li>Improved images for areo theme</li></ul></li></ul>"
+		),
+		'3.2.1'		=> array(
+			'branchName'	=> '3.2.1Update',
+			'releaseNotes'	=> "<ul><li>Bug Fixes<ul><li>Fixed possible issue with update validation at end of update</li></ul></li></ul>"
 		),
 	)
 );
