ddc1159c9b92dd5e1a5ea6349620b77e6dac7549
Matt Reishman <matt.reishman@Me.com>
2017-12-12 13:45:42 -0600

    Added tmp log layout to advanced settings #104

--- settings/advanced.php
+++ settings/advanced.php
@@ -314,6 +314,33 @@ $countConfig--;
 						</select>
 					</div>
 				</li>
+				<li>
+					Log Layout
+					<?php $arrayOfwindowConfigOptions = array();
+					for ($i=0; $i < 3; $i++)
+					{
+						for ($j=0; $j < 3; $j++)
+						{
+							array_push($arrayOfwindowConfigOptions, "".($i+1)."x".($j+1));
+						}
+					}
+					?>
+					<div class="selectDiv">
+						<select name="windowConfig">
+							<?php foreach ($arrayOfwindowConfigOptions as $value)
+							{
+								$stringToEcho = "<option ";
+								if($value === $windowConfig)
+								{
+									$stringToEcho .= " selected ";
+								}
+								$stringToEcho .= " value=\"".$value."\"> ".$value."</option>";
+								echo $stringToEcho;
+							}
+							?>
+						</select>
+					</div>
+				</li>
 			</ul>
 		</div>
 		</form>
