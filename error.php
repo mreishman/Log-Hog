1e9713631184b107a7e4a99317e826f9da9a1276
Matt Reishman <matt.reishman@Me.com>
2017-12-20 09:55:50 -0600

    Starts changing error page #227

--- error.php
+++ error.php
@@ -11,13 +11,27 @@ require_once($baseUrl.'conf/config.php');
 require_once('core/conf/config.php');
 require_once('core/php/configStatic.php');
 require_once('core/php/commonFunctions.php');
+require_once('core/php/template/listOfFiles.php');
+
+$error = "";
+if(isset($_GET["error"]))
+{
+    $error = $_GET["error"];
+}
+
+$page = "";
+if(isset($_GET["page"]))
+{
+    $page = $_GET["page"];
+}
+
 ?>
 
-<h1> Error <?php echo $_GET["error"] ?> </h1>
-<h1> <?php echo $_GET["page"] ?> </h1>
+<h1> Error <?php echo $error ?> </h1>
+<h1> <?php echo $page ?> </h1>
 <img src="core/img/redWarning.png" height="60px">
 <?php
-if($_GET["error"] == 550)
+if($error == 550)
 {
 	echo "<h2>File Permission Error</h2>";
 	echo "Make sure the file permissions are set correctly for all of the files within loghog.";
@@ -30,24 +44,14 @@ if($_GET["error"] == 550)
 <p> File Permissions: </p>
 <?php
 
-$arrayOfFiles = array("update/updater.php","core/php/configStatic.php","core/php/loadVars.php","core/php/poll.php","core/php/settingsCheckForUpdate.php","core/php/settingsCheckForUpdateAjax.php","core/php/settingsSave.php","core/php/settingsInstallUpdate.php","core/php/updateActionFile.php","core/php/updateProgressFile.php","core/php/updateProgressFileNext.php","core/php/updateProgressLog.php","core/php/updateProgressLogHead.php","core/php/verifyWriteStatus.php");
-
-foreach ($arrayOfFiles as $key)
+foreach ($fileNameArray as $key => $value)
 {
 
-    $info = filePermsDisplay($key);
+    $info = filePermsDisplay($value["path"]);
 
     echo "<p>";
-    if((strpos(substr($info, 0, -7), "w")) === false)
-    {
-    echo '<img src="core/img/redWarning.png" height="10px">';
-    }
-    else
-    {
-    echo '<img src="core/img/greenCheck.png" height="10px">';
-    }
     echo "  ";
-    echo $key;
+    echo $value["name"];
     echo "   -   ";
     echo $info;
     echo "</p>";
