33b40a7c12253ef81238463f0296f38dcf2ac799
Matt Reishman <matt.reishman@me.com>
2018-01-21 11:16:36 -0600

    #206

--- update/updateInProgress.php
+++ update/updateInProgress.php
@@ -1,13 +1,7 @@
 <?php
 require_once("../core/php/commonFunctions.php");
-$baseUrl = "../core/";
-if(file_exists('../local/layout.php'))
-{
-	$baseUrl = "../local/";
-	//there is custom information, use this
-	require_once('../local/layout.php');
-	$baseUrl .= $currentSelectedTheme."/";
-}
+$currentSelectedTheme = returnCurrentSelectedTheme();
+$baseUrl = "../local/".$currentSelectedTheme."/";
 require_once($baseUrl.'conf/config.php');
 require_once('../core/php/configStatic.php');
 require_once('../core/php/updateProgressFile.php');
