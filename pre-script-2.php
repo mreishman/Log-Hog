c1030703be77fea21e58c0955398998d5de03d59
Matt Reishman <mreishman@users.noreply.github.com>
2018-01-30 09:23:48 -0600

    Update pre-script-2.php

--- pre-script-2.php
+++ pre-script-2.php
@@ -4,6 +4,6 @@ if (!file_exists("../../../../core/Themes/Ocean/img/"))
 	mkdir("../../../../core/Themes/Ocean/img/", 0777, true);
 }
 sleep(3);
-echo json_encode("../../../../core/Themes/Ocean/img/");
+echo json_encode("../../core/Themes/Ocean/img/");
 //TRUE for no check, or filename / path for check
 ?>
