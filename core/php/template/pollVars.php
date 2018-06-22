f9e41b8d4ffdcf7d6b5831266460b9a67664ebf2
mreishman <matthew.reishman@goedekers.com>
2018-06-22 11:13:03 -0500

    fixed var display of dropdown for poll var

--- core/php/template/pollVars.php
+++ core/php/template/pollVars.php
@@ -103,8 +103,8 @@ Poll Settings
 		<span class="settingsBuffer">Show Php errors from file open fails:</span>
 		<div class="selectDiv">
 			<select name="showErrorPhpFileOpen">
-					<option <?php if($showErrorPhpFileOpen == 'true'){echo "selected";} ?> value="true">JS</option>
-					<option <?php if($showErrorPhpFileOpen == 'false'){echo "selected";} ?> value="false">PHP</option>
+					<option <?php if($showErrorPhpFileOpen == 'true'){echo "selected";} ?> value="true">True</option>
+					<option <?php if($showErrorPhpFileOpen == 'false'){echo "selected";} ?> value="false">False</option>
 			</select>
 		</div>
 	</li>
