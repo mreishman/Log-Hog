93c9a6c79e2a0550295803bf561d54f1314d8183
mreishman <matthew.reishman@goedekers.com>
2018-07-23 11:18:28 -0500

    WhatsNew indiv fix from 5.0

--- settings/header2.php
+++ settings/header2.php
@@ -38,7 +38,7 @@ $baseUrlImages = $localURL;
 ?>
 <script type="text/javascript">
 	var baseUrl = "<?php echo baseURL();?>";
-	var popupSettingsArray = JSON.parse('<?php echo json_encode($popupSettingsArray) ?>');
+	var popupSettingsArray = {};
 	var currentVersion = "<?php echo $configStatic['version']; ?>";
 	var newestVersion = "<?php echo $configStatic['newestVersion']; ?>";
 	var saveVerifyImage = <?php echo json_encode(generateImage(
