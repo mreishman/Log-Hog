5cab132a5a6a2e6608ca7a39fd4b2a5a20a27f02
mreishman <matthew.reishman@goedekers.com>
2018-07-25 14:40:25 -0500

    Start of changes to whatsNew

--- core/php/template/whatsNew.php
+++ core/php/template/whatsNew.php
@@ -11,13 +11,6 @@
 		</tr>
 
 		<tr>
-			<td>
-			</td>
-			<td>
-			</td>
-		</tr>
-
-		<tr>
 			<th colspan="2" style="padding: 10px">
 				<h1>5.0</h1>
 				<h3>Multi-Log</h3>
@@ -413,230 +406,69 @@
 				?>
 			</td>
 		</tr>
-
-		<tr>
-			<th colspan="2" style="border-top: 1px solid white; padding: 10px">
-				<h1>3.2</h1>
-				<h3>Found it!</h3>
-			</th>
-		</tr>
-
-		<tr>
-			<td>
-				<b>SeleniumMonitor</b>
-				<ul>
-					<li>
-					Monitor your selenium grid, and run new tests from a web interface
-					</li>
-				</ul>
-				<br>
-				<b>Title Filter</b>
-				<ul>
-					<li>
-						Filter logs by title / path.
-					</li>
-				</ul>
-				<br>
-				<b>Config file versions</b>
-				<ul>
-					<li>
-						Restore versions of config.
-					</li>
-				</ul>
-			</td>
-			<td>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.2-1.png"
-						)
-					);
-				?>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.2-2.png"
-						)
-					);
-				?>
-				<br>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.2-3.png"
-						)
-					);
-				?>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.2-4.png"
-						)
-					);
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<th colspan="2" style="border-top: 1px solid white; padding: 10px">
-				<h1>3.1</h1>
-				<h3>Couldn't find it</h3>
-			</th>
-		</tr>
-
-		<tr>
-			<td>
-				<b>Search!</b>
-				<ul>
-					<li>
-					Run visual grep's from the new search addon
-					</li>
-				</ul>
-			</td>
-			<td>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.1-1.png"
-						)
-					);
-				?>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.1-2.png"
-						)
-					);
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<th colspan="2" style="border-top: 1px solid white; padding: 10px">
-				<h1>3.0</h1>
-				<h3>Stylin'</h3>
-			</th>
-		</tr>
-
-		<tr>
-			<td>
-				<b>Themes!</b>
-				<ul>
-					<li>
-					Change how Log-Hog looks by going to settings, then themes
-					</li>
-					<li>
-					3 new main themes
-					</li>
-					<li>
-					New customizability of the current and new themes.
-					</li>
-				</ul>
-			</td>
-			<td>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.0-1.png"
-						)
-					);
-				?>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.0-2.png"
-						)
-					);
-				?>
-				<br>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.0-3.png"
-						)
-					);
-				?>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/3.0-4.png"
-						)
-					);
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<th colspan="2" style="border-top: 1px solid white; padding: 10px">
-				<h1>2.3</h1>
-				<h3>Processes</h3>
-			</th>
-		</tr>
-
-		<tr>
-			<td>
-				<b>Monitor</b>
-				<ul>
-					<li>
-					CPU usage
-					</li>
-					<li>
-					Ram / swap usage
-					</li>
-					<li>
-					Disk usage / IO
-					</li>
-					<li>
-					PHP User time used / system time used
-					</li>
-					<li>
-					Network Interface receive / transmit
-					</li>
-					<li>
-					Shows list of processes
-					</li>
-				</ul>
-			</td>
-			<td>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/2.3-1.png"
-						)
-					);
-				?>
-				<?php echo generateImage(
-					$arrayOfImages["loadingImg"],
-					$imageConfig = array(
-						"class"		=>	"whatsNewImage",
-						"style"		=>	"width: 45%;",
-						"data-src"	=>	$imageDirModifierAbout."core/img/2.3-2.png"
-						)
-					);
-				?>
-			</td>
-		</tr>
+		<?php
+		$dataForWhatsNew = array(
+			1	=>	array(
+				"Version"		=>	"2.3",
+				"Name"			=>	"Processes",
+				"BP"			=>	array("CPU usage","Ram / swap usage","Disk usage / IO","PHP User time used / system time used","Network Interface receive / transmit","Shows list of processes"),
+				"Images"		=>	array("2.3-1.png","2.3-2.png")
+			),
+			2	=>	array(
+				"Version"		=>	"3.0",
+				"Name"			=>	"Stylin'",
+				"BP"			=>	array("Change how Log-Hog looks by going to settings, then themes","3 new main themes","New customizability of the current and new themes."),
+				"Images"		=>	array("3.0-1.png","3.0-2.png","3.0-3.png","3.0-4.png")
+			),
+			3	=>	array(
+				"Version"		=>	"3.1",
+				"Name"			=>	"Couldn't find it",
+				"BP"			=>	array("Run visual grep's from the new search addon"),
+				"Images"		=>	array("3.1-1.png","3.1-2.png")
+			),
+			4	=>	array(
+				"Version"		=>	"3.2",
+				"Name"			=>	"Found it!",
+				"BP"			=>	array("Monitor your selenium grid, and run new tests from a web interface","Filter logs by title / path","Restore versions of config."),
+				"Images"		=>	array("3.2-1.png","3.2-2.png","3.2-3.png","3.2-4.png")
+			),
+		);
+		$dataForWhatsNew = array_reverse($dataForWhatsNew);
+		$first = false; //change to true when at top
+		foreach($dataForWhatsNew as $value): ?>
+			<tr>
+				<?php if($first):
+					$first = false; ?>
+				<th colspan="2" style="padding: 10px">
+				<?php else: ?>
+				<th colspan="2" style="border-top: 1px solid white; padding: 10px">
+				<?php endif; ?>
+					<h1><?php echo $value["Version"]; ?></h1>
+					<h3><?php echo $value["Name"]; ?></h3>
+				</th>
+			</tr>
+			<tr>
+				<td style="vertical-align: top;">
+					<ul>
+						<?php foreach ($value["BP"] as $BPValue): ?>
+							<li><?php echo $BPValue; ?></li>
+						<?php endforeach; ?>
+					</ul>
+				</td>
+				<td>
+					<?php foreach ($value["Images"] as $IMGValue): ?>
+						<?php echo generateImage(
+							$arrayOfImages["loadingImg"],
+							$imageConfig = array(
+								"class"		=>	"whatsNewImage",
+								"style"		=>	"width: 100%; max-width: 500px;",
+								"data-src"	=>	$imageDirModifierAbout."core/img/".$IMGValue
+								)
+							);
+						?>
+					<?php endforeach; ?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
 	</table>
 </div>
