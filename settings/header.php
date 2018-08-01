a2abd35be9873a6012882248e5ed2ac2f60a07a9
mreishman <matthew.reishman@goedekers.com>
2018-08-01 16:52:57 -0500

    updated header for vars

--- settings/header.php
+++ settings/header.php
@@ -67,6 +67,7 @@ echo loadSentryData($sendCrashInfoJS, $branchSelected); ?>
 		<a class="link" href="#settingsMenuVars" > Menu </a>
 		<a class="link" href="#settingsWatchlistVars" > Watchlist </a>
 		<a class="link" href="#settingsMultiLogVars" > Multi-Log </a>
+		<a class="link" href="#settingsInitialLoadLayoutVars"> Log Layout</a>
 		<a class="link" href="#settingsMainVars" > Other </a>
 	</div>
 <?php elseif(strpos($URI, 'themes.php') !== false): ?>
