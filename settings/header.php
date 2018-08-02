3a00530059c4797d98bae9f8b0fa862ad7214e3e
mreishman <matthew.reishman@goedekers.com>
2018-08-02 09:15:22 -0500

    fixed bug with multi log being disabled still showing multi log settings

--- settings/header.php
+++ settings/header.php
@@ -62,7 +62,9 @@ echo loadSentryData($sendCrashInfoJS, $branchSelected); ?>
 		<a class="link" href="#settingsUpdateVars" > Update </a>
 		<a class="link" href="#settingsMenuVars" > Menu </a>
 		<a class="link" href="#settingsWatchlistVars" > Watchlist </a>
-		<a class="link" href="#settingsMultiLogVars" > Multi-Log </a>
+		<?php if($enableMultiLog === "true"): ?>
+			<a class="link" href="#settingsMultiLogVars" > Multi-Log </a>
+		<?php endif; ?>
 		<a class="link" href="#settingsMainVars" > Other </a>
 	</div>
 <?php elseif(strpos($URI, 'themes.php') !== false): ?>
