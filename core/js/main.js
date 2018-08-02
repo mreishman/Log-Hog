90c822b93928963a0b876255546e232b72da8bd6
mreishman <matthew.reishman@goedekers.com>
2018-08-02 12:27:17 -0500

    fixed flicker issue

--- core/js/main.js
+++ core/js/main.js
@@ -2962,7 +2962,22 @@ function toggleGroupedGroups()
 	$(".allGroup").hide();
 	$(".active").show();
 	$("."+groupSelect+"Group").show();
+	//hide empty files if needed
+	hideEmptyLogs();
 	resize();
+}
+
+function hideEmptyLogs()
+{
+	var logKeys = Object.keys(logs);
+	var logKeysLength = logKeys.length;
+	for(var logHideCheck = 0; logHideCheck < logKeysLength; logHideCheck++)
+	{
+		if(logs[logKeys[logHideCheck]] === "<div class='errorMessageLog errorMessageGreenBG' > This file is empty. </div>")
+		{
+			hideLogByName(logKeys[logHideCheck]);
+		}
+	}
 }
 
 function onScrollShowFixedMiniBar(idsOfForms)
