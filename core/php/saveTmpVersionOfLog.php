0ec19a9cc4a433f9ad17d2a053897ae945a6eec7
mreishman <matthew.reishman@goedekers.com>
2018-08-07 14:48:10 -0500

    save works, sorta

--- core/php/saveTmpVersionOfLog.php
+++ core/php/saveTmpVersionOfLog.php
@@ -1,7 +1,9 @@
 <?php
 $location = $_POST['subFolder'];
-$fileName = implode("_", explode("/", $_POST["key"])).".php";
+$fileName = implode("_DIR_", explode("/", $_POST["key"])).".php";
 $fileContents = "<?php $"."logData = ".json_encode($_POST["log"]."; ?>");
-
-file_put_contents("../../".$location.$fileName, $fileContents);
+if(is_dir("../../".$location))
+{
+	file_put_contents("../../".$location."Backup".$fileName, $fileContents);
+}
 echo json_encode(true);
