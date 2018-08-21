42bb37f8778458929aaa3a73e31525e882b92a23
Matt Reishman <mreishman@users.noreply.github.com>
2018-08-21 09:45:04 -0500

    Add files via upload

---
+++ core_php_deleteLog.php
@@ -0,0 +1,41 @@
+<?php
+$verifyFile = $_POST['file'];
+require_once('../../local/layout.php');
+$baseUrl = "../../local/".$currentSelectedTheme."/";
+require_once($baseUrl.'conf/config.php');
+
+foreach($config['watchList'] as $value)
+{
+	$path = $value["Location"];
+	$filter = $value["Pattern"];
+	if(is_dir($path))
+	{
+		//folder
+		$path = preg_replace('/\/$/', '', $path);
+		$files = scandir($path);
+		if($files) {
+			unset($files[0], $files[1]);
+			foreach($files as $k => $filename) {
+				$fullPath = $path . '/' . $filename;
+				if(preg_match('/' . $filter . '/S', $filename) && is_file($fullPath))
+				{
+					if($verifyFile == $fullPath)
+					{
+						unlink($_POST['file']);
+						echo json_encode($_POST['file']);
+						break;
+					}
+				}
+			}
+		}
+	}
+	elseif(file_exists($path))
+	{
+		if($path == $verifyFile)
+		{
+			unlink($_POST['file']);
+			echo json_encode($_POST['file']);
+			break;
+		}
+	}
+}
