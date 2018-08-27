ef7d68f0956e46e000cf1650442200a2e627a112
mreishman <matthew.reishman@goedekers.com>
2018-08-27 08:33:23 -0500

    last fix for remove / delete

--- core/js/main.js
+++ core/js/main.js
@@ -2194,6 +2194,10 @@ function scrollToBottom(idNum)
 
 function clearLogInner(title)
 {
+	if(title.indexOf(" | ") > -1)
+	{
+		title = title.split(" | ")[0];
+	}
 	var urlForSend = "core/php/clearLog.php?format=json";
 	title = filterTitle(title);
 	var data = {file: title};
@@ -2314,6 +2318,10 @@ function deleteLog(title)
 {
 	try
 	{
+		if(title.indexOf(" | ") > -1)
+		{
+			title = title.split(" | ")[0];
+		}
 		var urlForSend = "core/php/deleteLog.php?format=json";
 		title = title.replace(/\s/g, "");
 		var data = {file: title};
