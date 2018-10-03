746845b6572b1eebf86c1a8b3c1e611e2495bae9
mreishman <matthew.reishman@goedekers.com>
2018-10-03 09:58:27 -0500

    testing stuff

---
+++ develop/tests/unit/testTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace develop\tests\unit;
+
+class testTest
+{
+	public function render();
+	{
+		return "test";
+	}
+
+	public function returnTrue()
+	{
+		return true;
+	}
+
+	public function testArray()
+	{
+		return array("test1",'test2');
+	}
+}
