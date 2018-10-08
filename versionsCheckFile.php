154a03642c0953f1209dfedf5f37448adc02ad97
Matt Reishman <mreishman@users.noreply.github.com>
2018-10-08 08:32:40 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -221,7 +221,7 @@ $versionCheckArray = array(
 		),
 		'5.1.9'		=> array(
 			'branchName'	=> '5.2BetaUpdate',
-			'image'		=> 'raw.githubusercontent.com/mreishman/Log-Hog/updateImages/5.2.png',
+			'image'		=> 'https://raw.githubusercontent.com/mreishman/Log-Hog/updateImages/5.2.png',
 			'releaseNotes'	=> "<ul><li>Features<ul><li>Cleaned up look of top bar by merging group settings and filter settings together</li><li>Moved themes page to main menu</li><li>Customize folder color schemes, add and edit new color schemes for files and folders</li><li>Save current layout as on load layout from index page</li></ul></li><li>Bug Fixes<ul><li>Merged update / update notice icons together for somewhat cleaner look</li><li>Better support for smaller resolution screens in full screen menu</li><li>Changed loading method for javascript, adding in better error detection</li><li>More images now loaded on-demand (less images in initial load, of which some were not needed - determined by custom settings)</li><li>Small style changes to updater</li><li>Fixed bug with updater check, and checking for update when update check in progress</li></ul></li></ul>"
 		),
 	)
