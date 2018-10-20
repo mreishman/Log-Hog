d0339745c1f045f28f15e37ed8a17d81ed3378cc
Matt Reishman <matt.reishman@me.com>
2018-10-20 17:15:31 -0500

    #542 from php to js
    - Moved js out of php files
    - Condensed js for upgrade config / layout into one js file (all functions were same)

--- core/php/template/upgradeLayout.php
+++ core/php/template/upgradeLayout.php
@@ -72,149 +72,15 @@ $totalUpgradeScripts = floatval($layoutVersionToUpgradeTo) - floatval($layoutVer
 </body>
 
 <script src="../../../core/js/settings.js?v=<?php echo $jsVersion?>"></script>
-<script type="text/javascript"> 
-	var lock = false;
+<script src="../../../core/js/upgradeMain.js?v=<?php echo $jsVersion?>"></script>
+<script type="text/javascript">
 	var urlForSendMain0 = '../../../core/php/checkVersionOfLayout.php?format=json';
 	var urlForSendMain = '../../../core/php/upgradeScript/upgradeLayout-';
 	var urlForSendMain2 = '.php?format=json';
-	var globalVersionBase = 1;
 	<?php
 	echo "var startVersion = ".$layoutVersion.";";
 	echo "var endVersion = ".$layoutVersionToUpgradeTo.";";
+	echo "var upgradeConfigUrlToRedirectTo = \"getCookieRedirect();\"";
 	?>
-	var verifyCountSuccess = 0;
-	$( document ).ready(function()
-	{
-		if(endVersion > startVersion)
-		{
-			runScript(startVersion+1);
-		}
-		else
-		{
-			window.location.href = "../../../settings/whatsNew.php";
-		}
-	});
-
-	function runScript(version)
-	{
-		document.getElementById("runCount").innerHTML = globalVersionBase;
-		document.getElementById("verifyCount").innerHTML = globalVersionBase;
-		document.getElementById('runLoad').style.display = "block";
-		document.getElementById('verifyLoad').style.display = "none";
-		var urlForSend = urlForSendMain+version+urlForSendMain2;
-		var dataSend = {version: version};
-		$.ajax({
-			url: urlForSend,
-			dataType: 'json',
-			data: dataSend,
-			type: 'POST',
-			success: function(data)
-			{
-				verifyFile(data);
-			},
-			failure: function(data)
-			{
-				runScript(startVersion+1);
-			}
-		});
-	}
-
-
-	function verifyFile(version)
-	{
-		document.getElementById('runCheck').style.display = "block";
-		document.getElementById('runLoad').style.display = "none";
-		document.getElementById('verifyLoad').style.display = "block";
-		verifyCount = 0;
-		verifyCountSuccess = 0;
-		verifyFileTimer = setInterval(function(){verifyFilePoll(version);},2000);
-	}
-
-	function verifyFilePoll(version)
-	{
-		if(lock === false)
-		{
-			lock = true;
-			var urlForSend = urlForSendMain0;
-			var data = {version: version};
-			(function(_data){
-				$.ajax({
-					url: urlForSend,
-					dataType: 'json',
-					data: data,
-					type: 'POST',
-					success: function(data)
-					{
-						verifyPostEnd(data, _data);
-					},
-					failure: function(data)
-					{
-						verifyPostEnd(data, _data);
-					},
-					complete: function()
-					{
-						lock = false;
-					}
-				});	
-			}(data));
-		}
-	}
-
-	function verifyPostEnd(verified, data)
-	{
-		if(verified == true)
-		{
-			verifyCountSuccess++;
-			if(verifyCountSuccess >= successVerifyNum)
-			{
-				verifyCountSuccess = 0;
-				clearInterval(verifyFileTimer);
-				verifySucceded(data['lastAction']);
-			}
-		}
-		else
-		{
-			verifyCountSuccess = 0;
-			verifyCount++;
-			if(verifyCount > 29)
-			{
-				clearInterval(verifyFileTimer);
-				verifyFail(data['lastAction']);
-			}
-		}
-	}
-
-	function updateError()
-	{
-		document.getElementById('innerDisplayUpdate').innerHTML = "<h1>An error occured while trying to upgrade layout file. </h1>";
-	}
-
-	function verifyFail(action)
-	{
-		updateError();
-	}
-
-	function verifySucceded(action)
-	{
-		retryCount = 0;
-		startVersion++;
-		globalVersionBase++;
-		if(endVersion > startVersion)
-		{
-			runScript(startVersion+1);
-		}
-		else
-		{
-			finishedTmpUpdate();
-		}
-	}
-
-	function finishedTmpUpdate()
-	{
-		document.getElementById('verifyCheck').style.display = "block";
-		document.getElementById('verifyLoad').style.display = "none";
-		window.location.href = "<?php echo getCookieRedirect(); ?>";
-	}
-
-</script> 
+</script>
 </html>
