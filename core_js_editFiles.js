41294ac659dcd6bdaf3654ef2d57e9dc5571590e
Matt Reishman <matt.reishman@me.com>
2018-11-10 08:47:53 -0600

    Added update files

---
+++ core_js_editFiles.js
@@ -0,0 +1,37 @@
+$(document).ready(function()
+{
+	showFile(newValue, "LocalConfig");
+
+	var targetHeight = window.innerHeight - $("#fixed").outerHeight() - 10;
+	$("#scrollable").outerHeight(targetHeight);
+
+	var targetWidth = window.innerWidth - $("#leftCol").outerWidth() - 30;
+	$("#rightCol").outerWidth(targetWidth);
+	$("#document").outerWidth(targetWidth);
+});
+
+function loadFile(filepath, name)
+{
+	var urlForSend = "../core/php/returnFileContents.php?format=json";
+	var data = {file: "../../"+filepath};
+	$.ajax(
+	{
+		url: urlForSend,
+		data,
+		name,
+		type: "POST",
+		success(data)
+		{
+			showFile(data, this.name);
+		},
+		error(data, data2){},
+		complete(){}
+	});
+}
+
+function showFile(data, name)
+{
+	$(".documentLink").removeClass("active");
+	$("#"+name+"Link").addClass("active");
+	document.getElementById("document").innerHTML = data;
+}
---
+++ core_js_updateInProgress.js
@@ -0,0 +1,87 @@
+var counter = 0;
+var counterInt = null;
+
+$( document ).ready(function()
+{
+	counterInt = setInterval(checkIfChange, 3000);
+
+});
+
+function checkIfChange()
+{
+	var urlForSend = "../core/php/getPercentUpdate.php?format=json";
+	var data = {};
+	$.ajax({
+		url: urlForSend,
+		dataType: "json",
+		data: data,
+		type: "POST",
+		success(data)
+		{
+			var newPercent = parseInt(data);
+			document.getElementById("innerSettingsText").innerHTML = "<br> Current Percent: "+currentPercent+"% ("+counter+")"+document.getElementById("innerSettingsText").innerHTML;
+			if(newPercent === currentPercent)
+			{
+				counter++;
+				if(counter > 40)
+				{
+					window.location.href = "../settings/update.php";
+				}
+				else if(currentPercent === 100)
+				{
+					document.getElementById("innerSettingsText").innerHTML = "<br> Finished Update";
+					finishedUpdate();
+					clearInterval(counterInt);
+				}
+				else
+				{
+					updateCounter();
+				}
+			}
+			else
+			{
+				counter = 0;
+				document.getElementById("progressBar").value = newPercent;
+				currentPercent = newPercent;
+				if(currentPercent === 100)
+				{
+					finishedUpdate();
+					clearInterval(counterInt);
+				}
+				else
+				{
+					updateCounter();
+				}
+			}
+		}
+	});
+}
+
+function updateCounter()
+{
+	var textToUpdateTo = "2 Minutes";
+	var counterInner = counter;
+	if(counterInner !== 0)
+	{
+		if(counter <= 20)
+		{
+			textToUpdateTo = "1 Minute ";
+		}
+		else
+		{
+			textToUpdateTo = "";
+			counterInner -= 20;
+		}
+		if(counterInner !== 0)
+		{
+			textToUpdateTo += ((20-counterInner) * 3) + " Seconds";
+		}
+	}
+	document.getElementById("counterDisplay").innerHTML = textToUpdateTo;
+}
+
+function finishedUpdate()
+{
+	document.getElementById("titleForUpdater").innerHTML = "Finished Update";
+	document.getElementById("innerDisplayUpdate").innerHTML = "<a class='link' onclick='window.location.href = \"../settings/update.php\"'  >Back to Log-Hog</a> ";
+}
