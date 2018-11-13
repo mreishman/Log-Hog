0ab6205eaca59c710f5551862d2ce6ce71420cfe
mreishman <matthew.reishman@goedekers.com>
2018-11-13 13:48:58 -0600

    php message render sorta works, sometimes

--- core/js/format.js
+++ core/js/format.js
@@ -125,18 +125,120 @@ function formatMainMessage(message, extraData)
 function formatPhpMessage(message, extraData)
 {
 	var message = message.split("PHP message:");
-	var severity = getPhpSeverifyLevel(message[1])
-	var restOfMessage = message[1].split(":");
+	var firstPartOfMessage = message[0];
+	message.shift();
+	if(typeof message !== "string")
+	{
+		message = message.join("PHP message:");
+	}
+	var restOfMessage = message.split(":");
 	var messageWarning = restOfMessage[0];
+	var severity = getPhpSeverifyLevel(messageWarning);
 	restOfMessage.shift();
 	restOfMessage = restOfMessage.join(":");
-	restOfMessage = formatMainMessage(restOfMessage, extraData);
-	var firstPartOfMessage = message[0];
+	restOfMessage = parseErrorMessage(restOfMessage, extraData);
 	if(firstPartOfMessage !== "")
 	{
 		firstPartOfMessage = formatMainMessage(firstPartOfMessage, extraData);
 	}
-	return firstPartOfMessage+"<table style=\"width=100%;\" ><tr><td style=\"width: 15px;\" ><img src=\""+severity+"\" height=\"15px\" ></td><td>"+messageWarning+"</td></tr><tr><td colspan=\"2\" >"+restOfMessage+"</td></tr></table>";
+	return firstPartOfMessage+"<div><img src=\""+severity+"\" height=\"15px\" >"+messageWarning+"</div><div class=\"settingsDiv\" >"+restOfMessage+"</div>";
+}
+
+function parseErrorMessage(restOfMessage, extraData)
+{
+	//check for client and extra data after that
+	//0: main data, 1: client, 2: server, 3: request, 4: upstream, 5: host, 6: referrer
+	var arrayOfData = {
+		0: {
+			"string"	: restOfMessage,
+			"key"		: "",
+			"position"	: -1
+		},
+		1: {
+			"string"	: "",
+			"key"		: "Client:",
+			"position"	: restOfMessage.indexOf(", client:")
+		},
+		2: {
+			"string"	: "",
+			"key"		: "Server:",
+			"position"	: restOfMessage.indexOf(", server:")
+		},
+		3: {
+			"string"	: "",
+			"key"		: "Request:",
+			"position"	: restOfMessage.indexOf(", request:")
+		},
+		4: {
+			"string"	: "",
+			"key"		: "Upstream:",
+			"position"	: restOfMessage.indexOf(", upstream:")
+		},
+		5: {
+			"string"	: "",
+			"key"		: "Host:",
+			"position"	: restOfMessage.indexOf(", host:")
+		},
+		6: {
+			"string"	: "",
+			"key"		: "Referrer:",
+			"position"	: restOfMessage.indexOf(", referrer:")
+		},
+	};
+	var arrayOfDataKeys = Object.keys(arrayOfData);
+	var trimmedMainData = false;
+	var atLeastOnePresent = false;
+	var skipLogic = false;
+	for(var aodc = 1; aodc < 7; aodc++)
+	{
+		if(!skipLogic)
+		{
+			//start filter, get lowest position (ext -1) then trim that out (first lowest to second lowest)
+			var lowest = -1;
+			var lowestPos = 0;
+			for(var aodc2 = 1; aodc2 < 7; aodc2++)
+			{
+				if(arrayOfData[arrayOfDataKeys[aodc2]]["position"] < lowest || lowest === -1)
+				{
+					atLeastOnePresent = true;
+					lowest = arrayOfData[arrayOfDataKeys[aodc2]]["position"];
+					lowestPos = aodc2;
+				}
+			}
+			if(lowest !== -1)
+			{
+				//find second lowest, cut out that part anad add to array of data
+				arrayOfData[arrayOfDataKeys[lowestPos]]["position"] = -1;
+				var secondLowest = restOfMessage.length;
+				for(var aodc3 = 1; aodc3 < 7; aodc3++)
+				{
+					if(arrayOfData[arrayOfDataKeys[aodc3]]["position"] < secondLowest && arrayOfData[arrayOfDataKeys[aodc3]]["position"] !== -1)
+					{
+						secondLowest = arrayOfData[arrayOfDataKeys[aodc3]]["position"];
+					}
+				}
+				arrayOfData[arrayOfDataKeys[lowestPos]]["string"] = restOfMessage.substring(lowest + 1, secondLowest);
+				if(!trimmedMainData)
+				{
+					arrayOfData[arrayOfDataKeys[0]]["string"] = restOfMessage.substring(0, lowest);
+					trimmedMainData = true;
+				}
+			}
+			else
+			{
+				skipLogic = true;
+			}
+		}
+	}
+	var newHtmlToSend = "";
+	for(var aodc4 = 0; aodc4 < 7; aodc4++)
+	{
+		if(arrayOfData[arrayOfDataKeys[aodc4]]["string"] !== "")
+		{
+			newHtmlToSend += "<div>"+formatMainMessage(arrayOfData[arrayOfDataKeys[aodc4]]["string"].trim(), extraData)+"</div>";
+		}
+	}
+	return newHtmlToSend;
 }
 
 function getPhpSeverifyLevel(snippit)
