213c040ffc8bad38f4282c34aa0a776a381ce7b8
Matt Reishman <matt.reishman@me.com>
2018-12-02 15:35:10 -0600

    js bugs with no logs in array and config file gen

--- core/js/main.js
+++ core/js/main.js
@@ -3,7 +3,7 @@ var allLogOneLog = {};
 var arrayToUpdate = {};
 var arrayOfData1 = null;
 var arrayOfData2 = null;
-var arrayOfDataMain = null;
+var arrayOfDataMain = {};
 var arrayOfDataSettings = [];
 var arrayOfScrollHeaderUpdate = ["aboutSpanAbout","aboutSpanInfo","aboutSpanGithub"];
 var borderPadding = 0;
@@ -300,13 +300,10 @@ function pollTwo()
 				}
 				else if(data === [] || $.isEmptyObject(data))
 				{
-					if(document.getElementById("noLogToDisplay").style.display !== "block")
-					{
-						document.getElementById("noLogToDisplay").style.display = "block";
-					}
+					toggleDisplayOfNoLogs(true);
 					if(firstLoad)
 					{
-						firstLoadEndAction();
+						afterPollFunctionComplete();
 					}
 				}
 				else
@@ -388,10 +385,12 @@ function pollTwoPartTwo(data)
 		t2 = performance.now();
 		filesNew = Object.keys(data);
 		var backupArrayOfDataMain = arrayOfDataMain;
-		if(arrayOfDataMain !== null)
+		
+		var arrayOfDataMainKeys = Object.keys(arrayOfDataMain);
+		var arrayOfDataMainKeysLength = arrayOfDataMainKeys.length;
+		if(arrayOfDataMainKeysLength > 0)
 		{
-			var arrayOfDataMainKeys = Object.keys(arrayOfDataMain);
-			for (var i = arrayOfDataMainKeys.length - 1; i >= 0; i--) 
+			for (var i = arrayOfDataMainKeysLength - 1; i >= 0; i--) 
 			{
 				if(arrayOfDataMainKeys[i] in data || arrayOfDataMainKeys[i].indexOf("LogHog/Backup") > -1 || arrayOfDataMainKeys[i].indexOf("oneLog") > -1 )
 				{
@@ -400,6 +399,7 @@ function pollTwoPartTwo(data)
 				delete arrayOfDataMain[arrayOfDataMainKeys[i]];
 			}
 		}
+		
 
 		//check for all update force
 		var boolForAllUpdateForce = false;
@@ -697,19 +697,14 @@ function arrayOfDataMainDataFilter(data)
 	try
 	{
 		var filesInner = Object.keys(data);
-		if(arrayOfDataMain === null)
+		var filesInnerLength = filesInner.length;
+		for (var dataSwapCount = filesInnerLength - 1; dataSwapCount >= 0; dataSwapCount--)
 		{
-			arrayOfDataMain = data;
+			arrayOfDataMain[filesInner[dataSwapCount]] = data[filesInner[dataSwapCount]];
 		}
-		else
-		{
-			for (var dataSwapCount = filesInner.length - 1; dataSwapCount >= 0; dataSwapCount--)
-			{
-				arrayOfDataMain[filesInner[dataSwapCount]] = data[filesInner[dataSwapCount]];
-			}
-		}
+		
 
-		for (var lineCountUpdateCount = filesInner.length - 1; lineCountUpdateCount >= 0; lineCountUpdateCount--)
+		for (var lineCountUpdateCount = filesInnerLength - 1; lineCountUpdateCount >= 0; lineCountUpdateCount--)
 		{
 			if(data[filesInner[lineCountUpdateCount]]["lineCount"] !== "---")
 			{
@@ -728,7 +723,15 @@ function firstLoadEndAction()
 	firstLoad = false;
 	if(oneLogEnable === "true")
 	{
-		addOneLogData();
+		if(Object.keys(arrayOfDataMain).length < 1)
+		{
+			addOneLogData();
+			document.getElementById("oneLog").style.display = "none";
+		}
+		else
+		{
+			addOneLogData();
+		}
 	}
 	if(allLogsVisible === "true")
 	{
@@ -1685,14 +1688,14 @@ function checkNameCont(checkName, currentCheck)
 	return false;
 }
 
-function toggleDisplayOfNoLogs()
+function toggleDisplayOfNoLogs(force = false)
 {
 	try
 	{
-		if($("#menu .active").length === 0)
+		if($("#menu .active").length === 0 || force === true)
 		{
 			//if still none active, none to display - add popup here
-			if(document.getElementById("noLogToDisplay").style.display !== "block")
+			if(document.getElementById("noLogToDisplay").style.display !== "block" || document.getElementById("log").style.display !== "none")
 			{
 				document.getElementById("noLogToDisplay").style.display = "block";
 				document.getElementById("log").style.display = "none";
@@ -1701,7 +1704,7 @@ function toggleDisplayOfNoLogs()
 		else
 		{
 			//we do not need this, hide popup
-			if(document.getElementById("noLogToDisplay").style.display !== "none")
+			if(document.getElementById("noLogToDisplay").style.display !== "none" || document.getElementById("log").style.display !== "block")
 			{
 				document.getElementById("noLogToDisplay").style.display = "none";
 				document.getElementById("log").style.display = "block";
