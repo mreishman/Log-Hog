3f2806f779c3efa6a780c0365893dbb41f8a8d8c
Matt Reishman <mreishman@users.noreply.github.com>
2019-09-02 15:10:09 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -1,7 +1,7 @@
 <?php
 
 $versionCheckArray = array(
-	'version'		=> '7.2',
+	'version'		=> '8.2',
 	'versionList'		=> array(
 		'2.0.1'	        => array(
 			'branchName'	=> '2.0.1Update',
@@ -325,7 +325,34 @@ $versionCheckArray = array(
 			'image'		=> 'https://raw.githubusercontent.com/mreishman/Log-Hog/updateImages/7.2.png',
 			'releaseNotes'	=> "<ul><li>Features<ul><li>Archive<ul><li>In main menu, the history tab was added for temporary log saves / archived logs</li><li>Added button in log window to show list of archived / temp history versions of the current selected log</li><li>Added button for saving current logs to archive</li><li>When saving logs (archive or temp) it now keeps backups of the files (up to a user defined limit)</li></ul></li><li>More information shown about updates (file size download, approx install file size)</li><li>Changing themes only refreshes the page once, instead of using a redirect</li></ul></li><li>Bug Fixes<ul><li>Update loading popup should display loading image</li><li>Watchlist file entry dropdown menu positioning fixed on initial load</li><li>Adding a backwards slash to folder names in watchlist now works correctly (and also filters)</li><li>Other various js fixes for file popup</li><li>Fixed bug where clear all logs would not remove notifications</li></ul></li></ul>"
 		),
-		
+		'7.3'			=> array(
+			'branchName'	=> '7.3Update',
+			'downloadSize'  => 687534,
+			'installSize'   => 9982370,
+			'image'		=> 'https://raw.githubusercontent.com/mreishman/Log-Hog/updateImages/7.3.png',
+			'releaseNotes'	=> "<ul><li>Features<ul><li>Faster render times for logs with new format enabled</li></ul></li><li>Bug Fixes<ul><li>Fixed bug in setup where addons would not download correctly</li><li>Fixed bug with loading image on about page / whats new page</li><li>Added easier option to enable or disable date text format for logs</li><li>Fixed window position on update in progress screen</li><li>Fixed bug in update logic when trying to revert changes</li><li>Fixed bug on static update page with popup logic. Should work smoother.</li><li>Small style tweaks / changes</li><li>Fixed bug with remove all archive logs button not working</li><li>Updated update notice logic to show difference of install size if less after update</li></ul></li></ul>"
+		),
+		'8.0'			=> array(
+			'branchName'	=> '8.0Update',
+			'downloadSize'  => 1194861,
+			'installSize'   => 10602634,
+			'image'		=> 'https://raw.githubusercontent.com/mreishman/Log-Hog/updateImages/8.0.png',
+			'releaseNotes'	=> "<ul><li>Features<ul><li>Visual Changes<ul><li>Combined loading bars for files and logs into one</li><li>Changed look of fullscreen notifications window to show more notifications</li><li>Changed look of inline notifications popup to show more information</li><li>Search is minimized into icon until click (better for smaller resolution screens)</li><li>Changed look of upgrade script / update pages.</li><li>Small visual changes to main menu</li></ul></li><li>Added more info on error page</li><li>Added option to select default initial page opened in full screen menu</li></ul></li><li>Bug Fixes<ul><li>Group select now uses either ctrl or command / meta key</li><li>Log width now resizes properly on window resize.</li><li>Bugfixes to settings watchlist</li><li>Hides log line load counter when finished loading log lines</li><li>Fixed bug with log list sidebar menu staying open with main log menu position of left or right</li></ul></li></ul>"
+		),
+		'8.1'			=> array(
+			'branchName'	=> '8.1Update',
+			'downloadSize'  => 773325,
+			'installSize'   => 10545145,
+			'image'		=> 'https://raw.githubusercontent.com/mreishman/Log-Hog/updateImages/8.1.png',
+			'releaseNotes'	=> "<ul><li>Features<ul><li>Settings pages moved to main menu</li></ul></li><li>Bug Fixes<ul><li>Slightly quicker load times with css because of minification</li><li>Fixed issues with Firefox not using custom progress bar styles on update page</li><li>Saving general theme options or folder group color options now correctly fades out success message popup</li><li>Per window filter now hidden if filters disabled</li><li>Log will properly resize when log menu on left or right</li></ul></li></ul>"
+		),
+		'8.2'			=> array(
+			'branchName'	=> '8.2Update',
+			'downloadSize'  => 679808,
+			'installSize'   => 9838503,
+			'image'		=> 'https://raw.githubusercontent.com/mreishman/Log-Hog/updateImages/8.2.png',
+			'releaseNotes'	=> "<ul><li>Features<ul><li>Pause individual logs from log sidebar</li><li>Refresh individual logs from log sidebar</li><li>Added a button at the top of logs allow for loading more lines of the current log (increase line number by current slice size)</li><li>Added sidebar buttons for preset groups (click to auto select a group of groups). Save current selection with save buttons below.</li><li>Added setting to always load last sessions logs / layout on initial load.</li><li>Added setting to invert the direction of logs (top to bottom or bottom to top).</li><li>Added scroll to top button</li></ul></li><li>Bug Fixes<ul><li>Reduced size of Log-Hog install by removing duplicate images and inverting them in css</li><li>Correct images should load on theme change without cache clear</li><li>Added button to close search on index when open</li><li>New error type added to update check download action</li></ul></li></ul>"
+		),
 	)
 );
 
